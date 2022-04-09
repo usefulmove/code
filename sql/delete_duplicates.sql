2cf9a419bd9a1630b3b792790f3210cc84c1f8eb
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-04-08 22:51:25 -0700

    sql: adding delete duplicates library SQL template

---
+++ sql/delete_duplicates.sql
@@ -0,0 +1,29 @@
+-- create table for example
+if exists temp_table
+  drop table temp_table ;
+create table temp_table (
+  id integer not null auto_increment,
+  dept varchar(32),
+  primary key (id)
+) ;
+alter table temp_table auto_increment=1 ;
+insert into temp_table
+  (dept)
+  select dept from survey_one ;
+
+
+-- delete duplicate records from table
+delete from temp_table
+where id not in
+        ( -- minimum ids for each grouped duplicate addresses
+          select id from
+          (
+            select min(a.id) as id
+            from temp_table a
+             -- the ony reason for this join is to get around a MySQL
+             -- shortcoming that will not allow deletion on a table
+             -- that is used in the where clause
+             left join temp_table b on a.id = b.id
+            group by a.dept
+          ) as o
+        ) ;
