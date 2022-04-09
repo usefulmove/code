143a17f583370b3490be41e85f2d7776d8bc9b9a
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-04-09 09:11:35 -0700

    sql: minor update to comments

--- sql/library/delete_duplicates.sql
+++ sql/library/delete_duplicates.sql
@@ -16,11 +16,11 @@ insert into temp_table
 delete from temp_table
 where id not in
         ( -- minimum ids for each grouped duplicate addresses
-          select id from
+          select id from -- i don't understand why MySQL requires this
           (
             select min(a.id) as id
             from temp_table a
-             -- the ony reason for this join is to get around a MySQL
+             -- the reason for this join is to get around a MySQL
              -- shortcoming that will not allow deletion on a table
              -- that is used in the where clause
              left join temp_table b on a.id = b.id
