46add353a9eab3853f77de103c28886c92bc6784
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-04-22 00:15:58 -0700

    cores: m

--- cores/core.R
+++ cores/core.R
@@ -35,8 +35,8 @@ db_local <- dbConnect( # local MySQL database
   dbname = "minic",
   host = "127.0.0.1",
   port = 3306,
-  user = "root",
-  password = "rootroot"
+  user = "",
+  password = ""
 )
 
 dbWriteTable(
