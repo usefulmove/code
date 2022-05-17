2c8bdb1a588017b09fe0ce84bc9560a22c3e9cbb
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-05-16 22:05:42 -0700

    (automated gcp push)

--- pat/pat.sh
+++ pat/pat.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sed 's/(\([^ ]\)/( \1/g' $1 | sed 's/\([^ ]\))/\1 )/g' | sed 's/(\([^ ]\)/( \1/g' | sed 's/\([^ ]\))/\1 )/g' | sed 's/\s\+$//g'
+sed 's/(\([^ ]\)/( \1/g' $1 | sed 's/\([^ ]\))/\1 )/g' | sed 's/(\([^ ]\)/( \1/g' | sed 's/\([^ ]\))/\1 )/g' | sed 's/\s\+$//g' > .temporary_file
+
+cp .temporary_file $1
+
+rm .temporary_file
+
 
