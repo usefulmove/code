1fb3509dad4783c8c7a56c6c0b6d1525e6ffabc7
Duane Edmonds <duane.edmonds@gmail.com>
2022-05-20 18:33:49 -0700

    (automated gcp push)

--- snowflake/snowflake.R
+++ snowflake/snowflake.R
@@ -15,22 +15,26 @@ snow_db <-
     Schema       = "SALESFORCESNOWFLAKE"
   )
 
+from_date <- Sys.Date() - 7
+
 status <- 
   snow_db |> 
   DBI::dbGetQuery(
-    "
-      select
-        STATUS_C as status,
-        count(*) as count,
-        max(LAST_MODIFIED_DATE) as last_updated
-      from SALESFORCESNOWFLAKE.LIMS_SAMPLE_C
-      where
-        CREATED_DATE like '2022-05%'
-          and
-        STATUS_C in ('Released', 'Received', 'In Process', 'Retest', 'Rack Retest Required')
-      group by STATUS_C
-      order by count desc
-    "
+    stringr::str_glue(
+      "
+        select
+          STATUS_C as status,
+          count(*) as count,
+          max(LAST_MODIFIED_DATE) as last_updated
+        from SALESFORCESNOWFLAKE.LIMS_SAMPLE_C
+        where
+          CREATED_DATE > '{from_date}'
+            and
+          STATUS_C in ('Released', 'Received', 'In Process', 'Retest', 'Rack Retest Required')
+        group by STATUS_C
+        order by count desc
+      "
+    )
   )
 
 process_time <-
@@ -45,20 +49,23 @@ process_time <-
 snow_db |> DBI::dbDisconnect()
 
 # return
+c_released <- status |> filter(STATUS == "Released") |> pull(COUNT)
 c_accessioned <- status |> filter(STATUS == "Received") |> pull(COUNT)
 c_inprocess <- status |> filter(STATUS == "In Process") |> pull(COUNT)
 c_retest <- status |> filter(STATUS == "Retest") |> pull(COUNT)
 c_rackretest <- status |> filter(STATUS == "Rack Retest Required") |> pull(COUNT)
 last_update <- stringr::str_extract(max(status$LAST_UPDATED), ".*[^UTC]$")
 
-return_message <-
+msg <-
   stringr::str_glue("\n
-                     SB process time remaining: {format(process_time$sb_proces_time, digits = 3)} hours
+                     SB process time remaining: {format(process_time$sb_proces_time, digits = 3)} hours\n
+                       released: {c_released}
                        accessioned: {c_accessioned}
                        in process: {c_inprocess}
                        retest: {c_retest}
                        rack retest req.: {c_rackretest}
-                     \n
-                     (Updated: {last_update})
+                       (since {from_date})\n
+                     (updated: {last_update})
                      \n")
-print(return_message)
+
+print(msg)
