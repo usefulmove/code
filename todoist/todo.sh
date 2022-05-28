d572fa64a4828b3c3c1c25a8a6063459c62aa2b6
Duane <duane.edmonds@gmail.com>
2022-05-27 22:24:09 -0700

    (automated gcp push)

--- todoist/todo.sh
+++ todoist/todo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# add task argument as task to inbox using Todoist REST API
+
 DATA='{"content": "'
 DATA+="$1"
 DATA+='", "project_id": 2244893558}'
 
-# add task argument as task to inbox using Todoist REST API
 COMMAND='curl "https://api.todoist.com/rest/v1/tasks" -X POST --data '"'"''
 COMMAND+=$DATA
 COMMAND+=''"'"' -H "Content-Type: application/json" -H "X-Request-Id: $(uuidgen)" -H "Authorization: Bearer c0e02a566b8388261b77718ef2908c55cc774a81"'
