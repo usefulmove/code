fe64e5477362e40d7a8b63ace72ec511a806fb3c
Duane <duane.edmonds@gmail.com>
2022-06-08 21:33:51 -0700

    cointoss: added version written in Julia

---
+++ cointoss/cointoss.jl
@@ -0,0 +1,43 @@
+#!julia
+
+using Statistics
+
+const TOTAL_TOSSES = 1e9
+
+pattern_a = [false, true, true]
+pattern_b = [true, false, true]
+
+tosses = Vector{Bool}()
+
+
+pattern_a_counter = 0
+pattern_b_counter = 0
+
+pattern_a_found = Vector{Int64}()
+pattern_b_found = Vector{Int64}()
+
+for i in 1:TOTAL_TOSSES
+  # toss and increment counters
+  push!(tosses, rand(Bool))
+  global pattern_a_counter += 1
+  global pattern_b_counter += 1
+
+  #println("i = $i  tosses = $tosses  pattern_a_counter = $pattern_a_counter  pattern_b_counter = $pattern_b_counter")
+  # check for pattern
+  if tosses == pattern_a && pattern_a_counter >= length(pattern_a)
+    #println("match a - pattern_a_counter = $pattern_a_counter")
+    push!(pattern_a_found, pattern_a_counter)
+    pattern_a_counter = 0 # reset counter
+  elseif tosses == pattern_b && pattern_b_counter >= length(pattern_b)
+    #println("match b - pattern_b_counter = $pattern_b_counter")
+    push!(pattern_b_found, pattern_b_counter)
+    pattern_b_counter = 0 # reset counter
+  end
+
+  if length(tosses) == length(pattern_a)
+    popfirst!(tosses)
+  end
+end
+
+println(mean(pattern_a_found))
+println(mean(pattern_b_found))
