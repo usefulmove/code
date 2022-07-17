8ccfe52f8b9036e40e23674983f016d2a694176c
Duane <duane.edmonds@gmail.com>
2022-07-17 00:03:44 -0700

    sort: completed Rust quicksort implementation

--- sort/qs/src/main.rs
+++ sort/qs/src/main.rs
@@ -2,46 +2,56 @@ use rand;
 
 fn main() {
     // create vector of random integers
-    let o: &mut Vec<u32> = &mut Vec::new();
-    for _ in 0..5 {
+    let mut o: Vec<u32> = Vec::new();
+    for _ in 0..20 {
         o.push(rand::random::<u32>());
     }
-    //removelet o: &mut Vec<u32> = &mut vec![5, 2, 3, 1, 4];
-
 
     println!("orig: {:#?}", o);
-
-    qsort(o);
-
-    println!("qsort: {:#?}", o);
+    println!("qsort: {:#?}", qsort(o.clone()));
+    o.sort();
+    println!("sort (built-in): {:#?}", o);
 }
 
-fn qsort(vec: &mut Vec<u32>) {
-    if vec.len() > 1 {
+fn qsort(vec: Vec<u32>) -> Vec<u32>  {
+    let cop = &mut vec.clone();
+
+    if cop.len() <= 1 {
+        vec
+    } else {
         // use last element as pivot
-        //println!("pivot: {}", vec[vec.len()-1]); // remove
+        let pivot: u32 = cop[cop.len()-1];
         
         let mut ins: usize = 0;
-        for icomp in 0..=(vec.len()-2) {
-            //removeprintln!("in loop: comp={} ({}) ins={} ({})", icomp, vec[icomp], ins, vec[ins]);
-            if vec[icomp] < vec[vec.len()-1] {
-                swap(vec, icomp, ins);
+        for icomp in 0..=(cop.len()-2) {
+            if cop[icomp] < cop[cop.len()-1] {
+                swap(cop, icomp, ins);
                 ins += 1;
             }
         }
-        swap(vec, ins, vec.len()-1);
+        swap(cop, ins, cop.len()-1);
 
-        //qsort(&vec[0..ins-1]);
-        //qsort(&vec[ins+1..vec.len()-1]);
+        let left: Vec<u32>;
+        if ins == 0 {
+            left = Vec::new();
+        } else {
+            left = Vec::from_iter(cop[0..=ins-1].iter().cloned());
+        }
+        let right: Vec<u32> = Vec::from_iter(cop[ins+1..].iter().cloned());
+
+        let mut sorted = qsort(left);
+        sorted.push(pivot);
+        sorted.extend(qsort(right));
+
+        sorted
     }
+
 }
 
 fn swap(vec: &mut Vec<u32>, i: usize, j: usize) {
-    //println!("swapping (i={}) {} (j={}) and {}", i, vec[i], j, vec[j]); // remove
     if vec.len() >= i && vec.len() >= j {
         let o = vec[i];
         vec[i] = vec[j];
         vec[j] = o;
     }
-    //removeprintln!("post-swap: {:?}", vec.clone());
 }
