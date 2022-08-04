c750d4f7f3dfc21e2995f1094598a60bbc13a0f0
Duane <duane.edmonds@gmail.com>
2022-08-04 00:18:08 -0700

    dij: updated Rust code to build relation graph for Dijkstra algorithm

--- dij/src/dij.rs
+++ dij/src/dij.rs
@@ -1,18 +1,43 @@
 use std::collections::HashMap;
 
 fn main() {
-    // create graph
-    let mut graph: HashMap<&str, HashMap<&str, u32>> = HashMap::new();
+    // build relation graph
+    let mut relation_graph: HashMap<&str, HashMap<&str, u32>> = HashMap::new();
 
-    // add node data
-    graph.insert("book", HashMap::new());
-    graph["book"].insert("lp", 5);
-    graph["book"].insert("poster", 0);
+    relation_graph.insert("book", HashMap::new());
+    relation_graph.insert("lp", HashMap::new());
+    relation_graph.insert("poster", HashMap::new());
+    relation_graph.insert("bass", HashMap::new());
+    relation_graph.insert("drums", HashMap::new());
+    for (&node, obj) in relation_graph.iter_mut() {
+        match node {
+            "book" => {
+                obj.insert("lp", 5);
+                obj.insert("poster", 0);
+            },
+            "lp" => {
+                obj.insert("bass", 15);
+                obj.insert("drums", 20);
+            },
+            "poster" => {
+                obj.insert("bass", 30);
+                obj.insert("drums", 35);
+            },
+            "bass" => {
+                obj.insert("piano", 20);
+            },
+            "drums" => {
+                obj.insert("piano", 10);
+            },
+            _ => {
+            },
+        }
+    }
     
-    println!("{:#?}", graph);
+    println!("{:#?}", relation_graph);
 }
 
 #[test]
 fn test_test() {
-    assert!(graph.contains_key("book"));
+    // TODO
 }
