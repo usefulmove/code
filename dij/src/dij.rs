941d3ac5a0ae10bcdaa11b7bdab296a00654470b
Duane <duane.edmonds@gmail.com>
2022-08-04 01:38:42 -0700

    dij: continued build out of Rust implementation

--- dij/src/dij.rs
+++ dij/src/dij.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
     // build relation graph
@@ -35,6 +36,39 @@ fn main() {
     }
     
     println!("{:#?}", relation_graph);
+
+
+    /*
+
+      execute Dijkstra's algorithm on graph
+        to find shortest weighted path from
+        book to drums
+      
+      we have a weighted graph that represents ("holds") the transaction
+      (node - position, connections, distance) information for a connection
+      diagram that we will use Dijkstra's algorithm on to find the shortest
+      (lowest cost) path from start (book) to end (piano) node. the
+      transactions are the edges where the costs are the weights.
+
+    */
+
+    let start_node: &str = "book";
+    let end_node: &str = "piano";
+
+    //    all nodes
+    let mut all_nodes: HashSet<&str> = HashSet::new();
+    all_nodes.insert(end_node);
+    for o in relation_graph.keys() {
+        all_nodes.insert(o);
+    }
+
+    println!("all nodes: {:#?}", all_nodes);
+
+    //    processed status
+    let mut processed: HashSet<&str> = HashSet::new();
+    processed.insert(end_node);
+
+    println!("processed nodes: {:#?}", processed);
 }
 
 #[test]
