4ca93325f27bb86cdbd295bffc4807fdc0bf5299
Duane <duane.edmonds@gmail.com>
2022-08-16 00:59:53 -0700

    (automated gcp push)

--- rust/library/passing_closures/src/main.rs
+++ rust/library/passing_closures/src/main.rs
@@ -1,11 +1,11 @@
 struct B {
-    f: Box<dyn Fn(u8)>,
+    f: Box<dyn Fn(u8) -> u8>,
 }
 
 impl B {
     fn new<F>(f: F) -> Self
     where 
-        F: Fn(u8) + 'static,
+        F: Fn(u8) -> u8 + 'static,
     {
         B { f: Box::new(f) }
     }
@@ -15,15 +15,15 @@ impl B {
 fn main() {
     static n: u8 = 3;
 
-    let mut my_closure = B::new(|x| println!("{}", n + x + 1));
+    let mut my_closure = B::new(|x| n + x + 1);
 
     if true {
-        my_closure = B::new(|x| println!("{}", n + x + 2));
+        my_closure = B::new(|x| n + x + 2);
     }
     else {
-        my_closure = B::new(|x| println!("{}", n + x + 5));
+        my_closure = B::new(|x| n + x + 5);
     }
 
-    (my_closure.f)(2);
+    println!("{}", (my_closure.f)(2));
 }
 
