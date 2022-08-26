643037135452e99bf9586ddfd02ab71e3ba68e6a
Duane <duane.edmonds@gmail.com>
2022-08-26 00:31:40 -0700

    fib: updated Rust code

--- fib/fib.rs
+++ fib/fib.rs
@@ -2,14 +2,20 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:#?}", fib(args[1].parse::<u64>().unwrap()));
+    println!(
+        "{}",
+        fib(
+            args[1]
+                .parse::<u64>()
+                .unwrap()
+        )
+    );
 }
 
 fn fib(n: u64) -> u64 {
     match n {
-        0 => 0,
-        1 => 1,
-        2 => 1,
-        _ => fib(n-1) + fib(n-2)
+        0     => 0,
+        1 | 2 => 1,
+        _     => fib(n-1) + fib(n-2),
     }
 }
