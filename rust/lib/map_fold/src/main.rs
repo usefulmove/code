109efe69bfd4cb525d810b86f8ce4ad8364ad1bb
Duane <duane.edmonds@gmail.com>
2022-08-30 15:28:58 -0700

    minor refactor

--- rust/lib/map_fold/src/main.rs
+++ rust/lib/map_fold/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let data: Vec<i32> = vec!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    println!("{:#?}", map_reduce(data));
+}
+
+fn map_reduce(data: Vec<i32>) -> i32 {
     let map: Vec<i32> =
         data
             .iter()
@@ -9,5 +13,5 @@ fn main() {
         map
             .iter()
             .fold(0, |acc, x| acc + x);
-    println!("{:#?}", reduce);
+    reduce
 }
