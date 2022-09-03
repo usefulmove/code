a0a0b0cc9ed78a14e765cffb8054b9989c3d84e2
Duane <duane.edmonds@gmail.com>
2022-09-03 09:45:54 -0700

    (automated gcp push)

--- sud/src/main.rs
+++ sud/src/main.rs
@@ -1,7 +1,11 @@
-fn main() {
-    let sudoku: Sudoku = Sudoku::new();
+use std::fmt;
 
-    println!("{:#?}", sudoku.puzzle);
+fn main() {
+    let mut sud: Sudoku = Sudoku::new();
+
+    sud.puzzle[4][4] = 9;
+
+    println!("{}", sud);
 }
 
 struct Sudoku {
@@ -13,5 +17,23 @@ impl Sudoku {
         Self {
             puzzle: [[0; 9]; 9],
         }
+    }
+}
+
+impl fmt::Display for Sudoku {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "  {:?}\n  {:?}\n  {:?}\n  {:?}\n  {:?}\n  {:?}\n  {:?}\n  {:?}\n  {:?}",
+            self.puzzle[0],
+            self.puzzle[1],
+            self.puzzle[2],
+            self.puzzle[3],
+            self.puzzle[4],
+            self.puzzle[5],
+            self.puzzle[6],
+            self.puzzle[7],
+            self.puzzle[8],
+        )
     }
 }
