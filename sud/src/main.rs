7831135635519089bfbe3d5ca814bcd339e12c62
Duane <duane.edmonds@gmail.com>
2022-09-03 22:56:57 -0700

    (automated gcp push)

--- sud/src/main.rs
+++ sud/src/main.rs
@@ -1,5 +1,5 @@
-use std::fmt;
-use std::env;
+use std::collections::HashSet;
+use std::{env, fmt};
 
 fn main() {
     let mut sud: Sudoku = Sudoku::new();
@@ -10,22 +10,25 @@ fn main() {
     //println!("debug..args..{:?}", args);
 
     let mut level: usize = 0;
-    args.iter().enumerate().for_each(|(i, s)| {
+    args.iter().enumerate().for_each(|(j, s)| {
         if s == "_" {
-            sud.board[level][i % 9] = 0;
+            sud.board[level][j % 9] = 0;
         } else {
-            sud.board[level][i % 9] = s.parse::<u8>().unwrap();
+            sud.board[level][j % 9] = s.parse::<u8>().unwrap();
         }
-        if i % 9 == 8 { level += 1; }
+        if j % 9 == 8 { level += 1; }
     });
 
-    println!("{}", sud);
+    println!(
+        "initial board:\n{}",
+        sud
+    );
 
     while !sud.is_solved() {
         for i in 0..9 {
             for j in 0..9 {
                 if sud.board[i][j] == 0 {
-                    match sud.solve_loc(i, j) {
+                    match sud.solve_location(i, j) {
                         Some(value) => sud.board[i][j] = value,
                         None => (), // do nothing
                     }
@@ -33,10 +36,20 @@ fn main() {
             }
         }
     }
+
+    println!(
+        "solution board:\n{}",
+        sud
+    );
 }
 
 struct Sudoku {
     board: [[u8; 9]; 9],
+}
+
+#[derive(PartialEq, Eq)]
+enum Group {
+    A, B, C, D, E, F, G, H, I
 }
 
 impl Sudoku {
@@ -57,8 +70,61 @@ impl Sudoku {
         true
     }
 
-    fn solve_loc(&self, i: usize, j: usize) -> Option<u8> {
-        TODO
+    fn solve_location(&self, a: usize, b: usize) -> Option<u8> {
+        let mut column_set: Set = Set::new();
+        let mut row_set: Set = Set::new();
+        let mut group_set: Set = Set::new();
+
+        // build sets
+        for i in 0..9 {
+            for j in 0..9 {
+                // ignore empty cells
+                if self.board[i][j] == 0 { continue; }
+
+                // check column
+                if j == b {
+                    column_set.insert(self.board[i][j]);
+                }
+
+                // check row
+                if i == a {
+                    row_set.insert(self.board[i][j]);
+                }
+
+                // check group
+                if self.get_group(i, j) == self.get_group(a, b) {
+                    group_set.insert(self.board[i][j]);
+                }
+            }
+        }
+
+        // check for solution ( set comparison )
+        let complete_set: Set = Set::from([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        let check_set: Set = column_set
+            .union(&row_set)
+            .collect::<Set>()
+            .union(&group_set)
+            .collect::<Set>();
+
+        match check_set.len() {
+            8 => Some(complete_set.difference(&check_set).collect::<Vec<&u8>>()[0].clone()),
+            _ => None,
+        }
+    }
+
+    fn get_group(&self, a: usize, b: usize) -> Group {
+        match (a, b) {
+            (0..=2, 0..=2) => Group::A,
+            (0..=2, 3..=5) => Group::B,
+            (0..=2, 6..=8) => Group::C,
+            (3..=5, 0..=2) => Group::D,
+            (3..=5, 3..=5) => Group::E,
+            (3..=5, 6..=8) => Group::F,
+            (6..=8, 0..=2) => Group::G,
+            (6..=8, 3..=5) => Group::H,
+            (6..=8, 6..=8) => Group::I,
+            _ => panic!("invalid group"),
+        }
     }
 
 }
@@ -80,3 +146,5 @@ impl fmt::Display for Sudoku {
         )
     }
 }
+
+type Set = HashSet<u8>;
