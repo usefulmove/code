4a5b7beec9e67c86d0e2eb61b4bb4e3c20fedc83
Duane <duane.edmonds@gmail.com>
2022-10-02 22:22:10 -0700

    updated Scala code

--- fib/fib.scala
+++ fib/fib.scala
@@ -1,10 +1,12 @@
 object Fib {
     def main(args: Array[String]) = {
-        printf("%d\n", fib(10))
+        printf("%d\n", fib(args(0).toInt))
     }
 
     def fib(n: Int): Int = {
-        if (n < 2) { return n }
-        else { return fib(n - 1) + fib(n - 2) }
+        n match
+            case 0 => 0
+            case 1 | 2 => 1
+            case _ => fib(n - 1) + fib(n - 2)
     }
 }
