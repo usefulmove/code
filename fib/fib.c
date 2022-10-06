a06ff5d1a4b4d142a6273272a3d4ca0b52d7e49b
Duane Edmonds <duane.edmonds@gmail.com>
2022-10-06 11:41:54 -0700

    added C code

---
+++ fib/fib.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fib(int n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fib(n-1) + fib(n-2);
+  }
+}
+
+int main() {
+    printf("%d", fib(38));
+    return 0;
+}
