8d832ee1e3d9ace9912818b2ee8953e15e5c0867
Duane Edmonds <duane.edmonds@gmail.com>
2022-10-09 21:37:38 -0700

    added tail recursive functions in Python and Lua

--- fib/fib.py
+++ fib/fib.py
@@ -8,5 +8,13 @@ def fib(n):
   else:
     return fib(n-1) + fib(n-2)
 
+def fib2(n, a = 0, b = 1):
+  if n == 0:
+    return a
+  if n == 1:
+    return b
+  return fib2(n - 1, b, a + b)
+
+
 if len(sys.argv) > 1:
   print(str(fib(int(sys.argv[1]))))
