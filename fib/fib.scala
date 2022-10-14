0cc2ef9da47db6b5ff8ead073f3e8e4f553202ba
Duane <duane.edmonds@gmail.com>
2022-10-14 16:10:56 -0700

    updated to off-side rule indentation of Scala 3

--- fib/fib.scala
+++ fib/fib.scala
@@ -1,11 +1,8 @@
-object Fib {
-    def main(args: Array[String]) = {
-        printf("%d\n", fib(args(0).toInt))
-    }
+object Fib:
+  def main(args: Array[String]) =
+    printf("%d\n", fib(args(0).toInt))
 
-    def fib(n: Int): Int = {
-        n match
-            case 0 | 1 => n
-            case _     => fib(n-1) + fib(n-2)
-    }
-}
+  def fib(n: Int): Int =
+    n match
+      case 0 | 1 => n
+      case _     => fib(n-1) + fib(n-2)
