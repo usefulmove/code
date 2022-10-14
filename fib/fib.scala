d263aec49a046aa2c8132c2608d73c7c560eff35
Duane <duane.edmonds@gmail.com>
2022-10-14 16:24:58 -0700

    updated Scala code to add tail recursion option

--- fib/fib.scala
+++ fib/fib.scala
@@ -1,8 +1,14 @@
 object Fib:
   def main(args: Array[String]) =
-    printf("%d\n", fib(args(0).toInt))
+    printf("%d\n", fib2(args(0).toInt))
 
   def fib(n: Int): Int =
     n match
       case 0 | 1 => n
       case _     => fib(n-1) + fib(n-2)
+
+  def fib2(n: Int, a: Int = 0, b: Int = 1): Int =
+    n match
+      case 0 => return a
+      case 1 => return b
+      case _ => fib2(n-1, b, a+b)
