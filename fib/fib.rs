2498bc5d5fb6218b20dd976bdeb2de24bf03f7a4
Duane <duane.edmonds@gmail.com>
2022-10-15 11:26:55 -0700

    (automated gcp push)

--- fib/fib.rs
+++ fib/fib.rs
@@ -2,9 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", fib(args[1].parse().unwrap()));
+    println!("{}", fib2(args[1].parse().unwrap()));
 }
 
+/* recursive solution */
 fn fib(n: u64) -> u64 {
     match n {
         0 | 1 => n,
@@ -12,12 +13,15 @@ fn fib(n: u64) -> u64 {
     }
 }
 
+/* tail recursive solution */
 fn fib2(n: u64) -> u64 {
     fibonacci2(n, 0, 1)
 }
 
 fn fibonacci2(n: u64, a: u64, b: u64) -> u64 {
-    if n == 0 {return a}
-    if n == 1 {return b}
-    fibonacci2(n-1, b, a+b)
+    match n {
+        0 => a,
+        1 => b,
+        _ => fibonacci2(n-1, b, a+b),
+    }
 }
