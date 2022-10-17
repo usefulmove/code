570a0ab0842f0059f36105de25346aac1df8eb7e
Duane <duane.edmonds@gmail.com>
2022-10-17 13:13:37 -0700

    added combination generation code to Scala library

---
+++ scala/lib/combinations.scala
@@ -0,0 +1,24 @@
+@main
+def main(args: String*): Unit =
+  val nums = List[Int](3, 2, 1, 5, 4)
+
+  //nums
+  //.foreach(println)
+
+  /* generate all possibile combinations of elements */
+
+  val combinations = (1 until scala.math.pow(2, nums.length).toInt) // create n-bit bitfield with all possible bitmasks
+  .map(_.toBinaryString)
+  .map(mask => {
+    mask
+    .reverse
+    .zip(nums)
+    .foldLeft(List[Int]())((acc, a) => {
+      a._1 match
+        case '0' => acc
+        case _ => a._2 :: acc
+     })
+   })
+
+  combinations
+  .foreach(println)
