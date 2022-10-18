e1c071cabe6cf1be986519b48bb963d7de1510b4
Duane <duane.edmonds@gmail.com>
2022-10-17 22:57:35 -0700

    updated Rust algorithm

--- sort/qs/src/main.rs
+++ sort/qs/src/main.rs
@@ -16,38 +16,37 @@ fn main() {
 fn qsort(vec: Vec<u32>) -> Vec<u32>  {
     let cop = &mut vec.clone();
 
-    if cop.len() <= 1 {
-        vec
-    } else {
-        // use last element as pivot
-        let pindex: usize = cop.len()/2;
-        swap(cop, pindex, cop.len()-1);
-        let pivot: u32 = cop[cop.len()-1];
-        
-        let mut ins: usize = 0;
-        for icomp in 0..=(cop.len()-2) {
-            if cop[icomp] < cop[cop.len()-1] {
-                swap(cop, icomp, ins);
-                ins += 1;
+    match cop.len() {
+        0 | 1 => vec,
+        _ => {
+            // use last element as pivot
+            let pindex: usize = cop.len()/2;
+            swap(cop, pindex, cop.len()-1);
+            let pivot: u32 = cop[cop.len()-1];
+
+            let mut ins: usize = 0;
+            for icomp in 0..=(cop.len()-2) {
+                if cop[icomp] < cop[cop.len()-1] {
+                    swap(cop, icomp, ins);
+                    ins += 1;
+                }
             }
+            swap(cop, ins, cop.len()-1);
+
+            let left: Vec<u32>;
+            match ins {
+                0 => left = Vec::new(),
+                _ => left = Vec::from_iter(cop[0..=ins-1].iter().cloned()),
+            }
+            let right: Vec<u32> = Vec::from_iter(cop[ins+1..].iter().cloned());
+
+            let mut sorted = qsort(left);
+            sorted.push(pivot);
+            sorted.extend(qsort(right));
+
+            sorted
         }
-        swap(cop, ins, cop.len()-1);
-
-        let left: Vec<u32>;
-        if ins == 0 {
-            left = Vec::new();
-        } else {
-            left = Vec::from_iter(cop[0..=ins-1].iter().cloned());
-        }
-        let right: Vec<u32> = Vec::from_iter(cop[ins+1..].iter().cloned());
-
-        let mut sorted = qsort(left);
-        sorted.push(pivot);
-        sorted.extend(qsort(right));
-
-        sorted
     }
-
 }
 
 fn swap(vec: &mut Vec<u32>, i: usize, j: usize) {
