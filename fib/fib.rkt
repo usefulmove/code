169dc904cdbbd3ab2be34ff8749788f0731c74d8
Duane Edmonds <duane.edmonds@gmail.com>
2022-10-19 02:19:32 -0700

    added tail recursive functions to Racket code

--- fib/fib.rkt
+++ fib/fib.rkt
@@ -1,4 +1,11 @@
 (define (fib n)
   (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2)))))
+
+(define (fibonacci2 n a b)
+  (if (= n 0)
+    a
+    (if (= n 1)
+      b 
+      (fibonacci2 (- n 1) b (+ a b)))))
   
 (writeln (fib 38))
