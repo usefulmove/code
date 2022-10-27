249b4407ee4a3ed6cb2a59b0902950045db871af
Duane <duane.edmonds@gmail.com>
2022-10-26 20:49:39 -0700

    (automated gcp push)

--- scala/lib/binarySearch.scala
+++ scala/lib/binarySearch.scala
@@ -1,30 +1,26 @@
+import scala.annotation.tailrec
+
 object BinarySearch {
       @main
-      def main(target: String, args: String*) = {
-          val out = binSearch(args.map(s => s.toInt).toArray, target.toInt) match {
+      def main(target: String, args: String*) =
+          val out = binSearch(args.map(_.toInt).toArray, target.toInt) match
               case Right(ind) => ind
               case _ => -1
-          }
-
           println(out)
-      }
 
 
-      def binSearch(arr: Array[Int], target: Int): Either[Int, Int] = {
-          var (left, mid, right) = (0, 0, arr.length - 1)
+      def binSearch(arr: Array[Int], target: Int): Either[Int, Int] =
+        
+          @tailrec
+          def recurse(left: Int, right: Int): Either[Int, Int] =
+              val mid = (left + right) >> 1
+              return mid match
+                  case mid if (arr(mid) == target) => Right(mid)
+                  case mid if (left > right) => Left(mid)
+                  case _ => target > arr(mid) match
+                      case true => recurse(mid + 1, right)
+                      case _ => recurse(left, mid - 1)
 
-          while (left <= right) {
-              mid = ((left + right) >> 1) 
-              
-              if (arr(mid) == target) return Right(mid)
+          recurse(0, arr.length - 1)
 
-              target > arr(mid) match {
-                  case true => left = mid + 1
-                  case _ => right = mid - 1
-              }
-          }
-
-          return Left(mid)
-      }
-      
 }
