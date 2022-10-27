f2bc4fa4be2d7dc7e8ed7d497c623ffe0c25f8ca
Duane Edmonds <duane.edmonds@gmail.com>
2022-10-26 20:09:51 -0700

    refactored binary search algorithm to be (tail) recursive

---
+++ scala/lib/binary_search.scala
@@ -0,0 +1,30 @@
+object BinarySearch {
+      @main
+      def main(target: String, args: String*) = {
+          val out = binSearch(args.map(s => s.toInt).toArray, target.toInt) match {
+              case Right(ind) => ind
+              case _ => -1
+          }
+          println(out)
+      }
+
+
+      def binSearch(arr: Array[Int], target: Int): Either[Int, Int] = {
+        
+          def helper(left: Int, right: Int): Either[Int, Int] = {
+              val mid = ((left + right) >> 1) 
+              return mid match {
+                  case mid if (arr(mid) == target) => Right(mid)
+                  case mid if (left <= right) => {
+                      target > arr(mid) match
+                        case true => helper(mid + 1, right)
+                        case _ => helper(left, mid - 1)
+                  }
+                  case _ => Left(mid)
+              }
+          }
+
+          helper(0, arr.length - 1)
+      }
+
+}
