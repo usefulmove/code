21ba0d5fa103563eefb3fe9590e890e80620aff5
Duane <duane.edmonds@gmail.com>
2022-11-02 22:30:27 -0700

    added currying code to Scala lib

---
+++ scala/lib/currying.scala
@@ -0,0 +1,7 @@
+object Currying:
+    @main
+    def main(args: String*) =
+        val add = (x: Int) => (y: Int) => x + y
+        val add2 = add(2)
+        println(add2(3))
+        println(add(2)(3))
