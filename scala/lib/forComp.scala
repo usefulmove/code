2d5db6234188bae6721752eec2b1f4ebc6e2e0a5
Duane <duane.edmonds@gmail.com>
2022-11-03 22:39:56 -0700

    added for comprehension code to Scala lib

---
+++ scala/lib/forComp.scala
@@ -0,0 +1,10 @@
+object Comprehension:
+  @main
+  def main(args: String*) =
+    val squares =
+    for {
+      c <- 'a' to 'h'
+      r <- 1 to 8
+    } yield c.toString + r.toString
+
+    squares foreach {println}
