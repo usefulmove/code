f89d7929eaea77eb8212a282f5cfe71238924dc7
Duane <duane.edmonds@gmail.com>
2022-11-05 12:09:54 -0700

    added magic8 project

---
+++ magic/magic8.scala
@@ -0,0 +1,29 @@
+#!scala
+object magic8:
+    @main
+    def main(args: String*) =
+        val answers = List(
+            "it is certain",
+            "it is decidedly so",
+            "without a doubt",
+            "yes definitely",
+            "you may rely on it",
+            "as I see it, yes",
+            "most likely",
+            "outlook good",
+            "yes",
+            "signs point to yes",
+            "reply hazy, try again",
+            "ask again later",
+            "better not tell you now",
+            "cannot predict now",
+            "concentrate and ask again",
+            "don't count on it",
+            "my reply is no",
+            "my sources say no",
+            "outlook not so good",
+            "very doubtful",
+        )
+        val ans = answers(scala.util.Random nextInt answers.size)
+
+        println {s"  $ans"}
