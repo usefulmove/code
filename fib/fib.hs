63ab163471e6220c826f7e061fb7d1a59a8c83bd
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-11-24 12:11:33 -0600

    added more idiomatic Haskell code

--- fib/fib.hs
+++ fib/fib.hs
@@ -1,6 +1,4 @@
-main = do
-    let fib n = if n < 2
-        then n
-        else (fib (n - 1) + fib (n - 2))
-  
-    print(fib 10)
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
