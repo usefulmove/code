0ab48c33bc82ac6b24863cdf6f372e9981ef7693
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-11-26 14:18:04 -0600

    (automated gcp push)

--- scala/lib/qsort2.scala
+++ scala/lib/qsort2.scala
@@ -4,14 +4,14 @@ object QSort:
         val list = args map {_.toInt}
         qsort(list) foreach {println}
 
-    def qsort(seq: Seq[Int]): Seq[Int] =
-        var os = seq
-        os.length match
-            case 0 | 1 => os
+    def qsort(sequence: Seq[Int]): Seq[Int] =
+        var seq = sequence
+        seq.length match
+            case 0 | 1 => seq
             case _ =>
-                var piv = 0
-                for a <- 1 until os.length do
-                    if os(a) < os(piv) then
-                        os = os(a) +: (os.slice(0, a) ++ os.slice(a + 1, os.length))
+                var piv = 0 // pivot
+                for a <- 1 until seq.length do
+                    if seq(a) < seq(piv) then
+                        seq = seq(a) +: (seq.slice(0, a) ++ seq.slice(a + 1, seq.length))
                         piv += 1
-                qsort(os.slice(0, piv)) ++ Seq(os(piv)) ++ qsort(os.slice(piv + 1, os.length))
+                qsort(seq.slice(0, piv)) ++ Seq(seq(piv)) ++ qsort(seq.slice(piv + 1, seq.length))
