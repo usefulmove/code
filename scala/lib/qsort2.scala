ae66c869332c1a5bed4d874ca1dd9e62c0c3e3a9
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-11-26 14:22:18 -0600

    modified quicksort algorithm to be type-generic

--- scala/lib/qsort2.scala
+++ scala/lib/qsort2.scala
@@ -1,17 +1,18 @@
 object QSort:
     @main def main(args: String*): Unit =
         //val list = List(3,1,2,5,4)
-        val list = args map {_.toInt}
+        //val list = List(3.1,1,2.2,5.0,4)
+        val list = args map {_.toDouble}
         qsort(list) foreach {println}
 
-    def qsort(sequence: Seq[Int]): Seq[Int] =
+    def qsort[T](sequence: Seq[T])(implicit order: Ordering[T]): Seq[T] =
         var seq = sequence
         seq.length match
             case 0 | 1 => seq
             case _ =>
                 var piv = 0 // pivot
                 for a <- 1 until seq.length do
-                    if seq(a) < seq(piv) then
+                    if order.lt(seq(a), seq(piv)) then
                         seq = seq(a) +: (seq.slice(0, a) ++ seq.slice(a + 1, seq.length))
                         piv += 1
                 qsort(seq.slice(0, piv)) ++ Seq(seq(piv)) ++ qsort(seq.slice(piv + 1, seq.length))
