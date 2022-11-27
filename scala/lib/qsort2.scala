363de7701b4f2cc33e1444a8d1f95aba48650a16
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-11-26 20:06:06 -0600

    quicksort algorithm improvements

--- scala/lib/qsort2.scala
+++ scala/lib/qsort2.scala
@@ -1,20 +1,12 @@
 object QSort:
     @main def main(args: String*): Unit =
-        //val list = List(3,1,2,5,4)
-        //val list = List(3.1,1,2.2,5.0,4)
-        //val list = List("this", "is", "a", "test")
-        //val list = List('c', 'a', 'b', 'e', 'd')
         val list = args map {_.toDouble}
         qsort(list) foreach {println}
 
-    def qsort[T](sequence: Seq[T])(implicit order: Ordering[T]): Seq[T] =
-        var seq = sequence
+    def qsort[T](seq: Seq[T])(implicit order: Ordering[T]): Seq[T] =
         seq.length match
             case 0 | 1 => seq
             case _ =>
-                var piv = 0 // pivot
-                for a <- 1 until seq.length do
-                    if order.lt(seq(a), seq(piv)) then
-                        seq = seq(a) +: (seq.slice(0, a) ++ seq.slice(a + 1, seq.length))
-                        piv += 1
-                qsort(seq.slice(0, piv)) ++ Seq(seq(piv)) ++ qsort(seq.slice(piv + 1, seq.length))
+                val piv :: rest = seq : @unchecked
+                val (small, large) = rest partition {order.lt(_, piv)}
+                qsort(small) ++ Seq(piv) ++ qsort(large)
