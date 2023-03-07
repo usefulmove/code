10ae5310f4e67f838d8b6d0c49182a1298751d2b
Duane Edmonds <duane.edmonds@gmail.com>
2023-03-06 23:32:55 -0800

    updated JS

--- fib/fib.js
+++ fib/fib.js
@@ -1,22 +1,13 @@
 #!node
 
-function fib(n) {
-  if (n < 2) {
-    return n
-  } else {
-    return ( fib(n-1) + fib(n-2) )
+const fib = (n) => n < 2 ? n : fib(n-1) + fib(n-2);
+
+const fib2 = (n, a = 0, b = 1) => {
+  switch (n) {
+    case 0: return a;
+    case 1: return b;
+    default: return fib2(n-1, b, a+b);
   }
 }
-
-function fib2(n, a = 0, b = 1) {
-  if (n == 0) {
-    return a
-  } else if (n == 1) {
-    return b
-  } else {
-    return (fib2(n-1, b, a+b))
-  }
-}
-
 
 console.log(fib2(38));
