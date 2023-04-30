db6e98d33cd1cac133fe8487f69a59008da41fc1
Duane Edmonds <duane.edmonds@gmail.com>
2023-04-30 00:47:19 -0700

    explain: modified argument handling to make use of argparse package

--- openai/explain.py
+++ openai/explain.py
@@ -3,16 +3,20 @@
 import openai
 import os
 import sys
-
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())  # load local .env file into environment
 
+# load local .env file into environment
+load_dotenv(find_dotenv())
+
+# set OpenAI API key
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# system message
 system = """
     You are a very close friend of the user, sharing a long history together. As an expert in various subjects, you excel at breaking down complex concepts. However, you are cautious and avoid giving advice on topics beyond your expertise. Your explanations are concise, focusing on essential details for basic understanding, but you can provide in-depth explanations upon request. Your detailed explanations include examples and useful metaphors, when helpful.
 
     You have a background in education and learning and have a knack for explaining complex subject in an easy-to-understand manner.
+
 """
 
 
@@ -39,17 +43,24 @@ def explain_topic(topic, detailed=False):
     return get_completion(prompt)
 
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == "-v" or sys.argv[1] == "--verbose":
-        verbose = True
-        topic = sys.argv[2]
-    else:
-        verbose = False
-        topic = sys.argv[1]
+def main():
+    import argparse
 
-    print(f"Topic: {topic}")
+    # argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Provide a detailed explanation')
+    parser.add_argument('topic', help='Topic to be explained')
+    args = parser.parse_args()
 
-    if verbose:
-        print(f"Response (detailed): {explain_topic(topic, detailed=True)}")
+    print(f"Topic: {args.topic}")
+
+    if args.verbose:
+        print(
+            f"Response (detailed): {explain_topic(args.topic, detailed=True)}")
     else:
-        print(f"Response (concise): {explain_topic(topic)}")
+        print(f"Response (concise): {explain_topic(args.topic)}")
+
+
+if __name__ == "__main__":
+    main()
