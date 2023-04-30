3338470abd86dd1fde913c7cb98cb423ceb699f6
Duane Edmonds <duane.edmonds@gmail.com>
2023-04-30 00:36:03 -0700

    explain.py: added system message to shape explainer; refined code following code review

--- openai/explain.py
+++ openai/explain.py
@@ -9,9 +9,18 @@ _ = load_dotenv(find_dotenv())  # load local .env file into environment
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+system = """
+    You are a very close friend of the user, sharing a long history together. As an expert in various subjects, you excel at breaking down complex concepts. However, you are cautious and avoid giving advice on topics beyond your expertise. Your explanations are concise, focusing on essential details for basic understanding, but you can provide in-depth explanations upon request. Your detailed explanations include examples and useful metaphors, when helpful.
 
-def get_completion(prompt, model="gpt-3.5-turbo", temperature=0.2):
-    messages = [{"role": "user", "content": prompt}]
+    You have a background in education and learning and have a knack for explaining complex subject in an easy-to-understand manner.
+"""
+
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0.1):
+    messages = [
+        {"role": "system", "content": system},
+        {"role": "user", "content": prompt},
+    ]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -20,21 +29,10 @@ def get_completion(prompt, model="gpt-3.5-turbo", temperature=0.2):
     return response.choices[0].message["content"]
 
 
-def explain_topic(topic):
+def explain_topic(topic, detailed=False):
     prompt = f"""
-        Please provide a concise explanation for the topic below. If the topic
-        is a question, kindly answer it in a concise, straightforward, and
-        easy-to-understand manner.
-
-        ```{topic}```
-    """
-    return get_completion(prompt)
-
-
-def explain_topic_detailed(topic):
-    prompt = f"""
-        Please provide a detailed explanation for the topic below. If the topic
-        is a question, kindly answer it in a detailed and straightforward manner.
+        Please provide a {"detailed" if detailed else "concise"} explanation for the topic below. If the topic
+        is a question, kindly answer the question.
 
         ```{topic}```
     """
@@ -52,6 +50,6 @@ if len(sys.argv) > 1:
     print(f"Topic: {topic}")
 
     if verbose:
-        print(f"Response (detailed): {explain_topic_detailed(topic)}")
+        print(f"Response (detailed): {explain_topic(topic, detailed=True)}")
     else:
         print(f"Response (concise): {explain_topic(topic)}")
