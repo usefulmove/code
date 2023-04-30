7f4a50316d9f6471f604b6a1d694198eb09cda47
Duane Edmonds <duane.edmonds@gmail.com>
2023-04-30 01:16:45 -0700

    explain: added simple ( explain to a 2nd grader ) option

--- openai/explain.py
+++ openai/explain.py
@@ -13,9 +13,16 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # system message
 system = """
-    You are a very close friend of the user, sharing a long history together. As an expert in various subjects, you excel at breaking down complex concepts. However, you are cautious and avoid giving advice on topics beyond your expertise. Your explanations are concise, focusing on essential details for basic understanding, but you can provide in-depth explanations upon request. Your detailed explanations include examples and useful metaphors, when helpful.
+    You are a very close friend of the user, sharing a long history together.
+    As an expert in various subjects, you excel at breaking down complex
+    concepts. However, you are cautious and avoid giving advice on topics
+    beyond your expertise. Your explanations are concise, focusing on
+    essential details for basic understanding, but you can provide in-depth
+    explanations upon request. Your detailed explanations include examples
+    and useful metaphors, when helpful.
 
-    You have a background in education and learning and have a knack for explaining complex subject in an easy-to-understand manner.
+    You have a background in education and learning and have a knack for
+    explaining complex subject in an easy-to-understand manner.
 
 """
 
@@ -33,10 +40,12 @@ def get_completion(prompt, model="gpt-3.5-turbo", temperature=0.1):
     return response.choices[0].message["content"]
 
 
-def explain_topic(topic, detailed=False):
+def explain_topic(topic, detailed=False, simple=False):
     prompt = f"""
-        Please provide a {"detailed" if detailed else "concise"} explanation for the topic below. If the topic
-        is a question, kindly answer the question.
+        Please provide a {"detailed" if detailed else "concise"} explanation for
+        the topic below. If the topic is a question, kindly answer the question.
+
+        {"Explain the topic like I'm a second grader." if simple else ""}
 
         ```{topic}```
     """
@@ -48,18 +57,17 @@ def main():
 
     # argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Provide a detailed explanation')
-    parser.add_argument('topic', help='Topic to be explained')
+    parser.add_argument('topic', help='topic to be explained')
+    parser.add_argument('-v', '--verbose', action='store_true', help='provide a detailed explanation')
+    parser.add_argument('-s', '--simple', action='store_true', help='provide a simple explanation')
     args = parser.parse_args()
 
     print(f"Topic: {args.topic}")
 
     if args.verbose:
-        print(
-            f"Response (detailed): {explain_topic(args.topic, detailed=True)}")
+        print(f"Response (detailed): {explain_topic(args.topic, detailed=True, simple=args.simple)}")
     else:
-        print(f"Response (concise): {explain_topic(args.topic)}")
+        print(f"Response (concise): {explain_topic(args.topic, detailed=False, simple=args.simple)}")
 
 
 if __name__ == "__main__":
