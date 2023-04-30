5d215e5165331e412014009cdae4e8eac67f23de
Duane Edmonds <duane.edmonds@gmail.com>
2023-04-29 21:30:07 -0700

    modified explain to read prompt as command line argument

--- openai/explain.py
+++ openai/explain.py
@@ -1,5 +1,8 @@
+#!/home/dedmonds/repos/code/openai/venv/bin/python3
+
 import openai
 import os
+import sys
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
@@ -15,6 +18,7 @@ def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
     )
     return response.choices[0].message["content"]
 
-prompt = "Concisely explain what a multi-layer perceptron is."
-
-print(f"{get_completion(prompt)}")
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]
+    print(f"{sys.argv[1]}")
+    print(f"{get_completion(prompt)}")
