5b343681de5b3a89d174875a377bb4a84e447bfa
Duane Edmonds <duane.edmonds@gmail.com>
2023-04-29 22:50:55 -0700

    added verbose prompt option to explain.py

--- openai/explain.py
+++ openai/explain.py
@@ -5,22 +5,24 @@ import os
 import sys
 
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # load local .env file into environment
+_ = load_dotenv(find_dotenv())  # load local .env file into environment
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_completion(prompt, model="gpt-3.5-turbo", temperature=0.2): 
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0.2):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=temperature, 
+        temperature=temperature,
     )
     return response.choices[0].message["content"]
 
+
 def explain_topic(topic):
     prompt = f"""
-        Please provide a concise explanation for the topic below.  If the topic
+        Please provide a concise explanation for the topic below. If the topic
         is a question, kindly answer it in a concise, straightforward, and
         easy-to-understand manner.
 
@@ -28,7 +30,28 @@ def explain_topic(topic):
     """
     return get_completion(prompt)
 
+
+def explain_topic_detailed(topic):
+    prompt = f"""
+        Please provide a detailed explanation for the topic below. If the topic
+        is a question, kindly answer it in a detailed and straightforward manner.
+
+        ```{topic}```
+    """
+    return get_completion(prompt)
+
+
 if len(sys.argv) > 1:
-    topic = sys.argv[1]
+    if sys.argv[1] == "-v" or sys.argv[1] == "--verbose":
+        verbose = True
+        topic = sys.argv[2]
+    else:
+        verbose = False
+        topic = sys.argv[1]
+
     print(f"Topic: {topic}")
-    print(f"Response: {explain_topic(topic)}")
+
+    if verbose:
+        print(f"Response (detailed): {explain_topic_detailed(topic)}")
+    else:
+        print(f"Response (concise): {explain_topic(topic)}")
