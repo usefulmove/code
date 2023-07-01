ed9e7fc5ff4152c559c0ae3d23f4f633d26b67ff
Duane Edmonds <duane.edmonds@gmail.com>
2023-06-30 18:38:05 -0700

    cointoss: updated cointoss3 python code to be more generic and allow sequences of varied length and generate all possible match permutations

--- cointoss/cointoss3.py
+++ cointoss/cointoss3.py
@@ -7,19 +7,26 @@ from statistics import mean
 
 def main():
     COINTOSSES = 8_000_000
-    print(f'calculating average search lengths for {COINTOSSES:,} coin tosses...')
+    print(f'calculating average search lengths for {COINTOSSES:,} coin tosses')
 
-    pattern = r'011'
-    avg = calcAverageSearch(pattern, COINTOSSES)
-    print(f'average search length for {pattern} pattern: {avg:.3f}')
+    pattern_length = 4  # must be less than 10
 
-    pattern = r'010'
-    avg = calcAverageSearch(pattern, COINTOSSES)
-    print(f'average search length for {pattern} pattern: {avg:.3f}')
+    #pattern = r'011'
+    #avg = calcAverageSearch(pattern, COINTOSSES)
+    #print(f'average search length for {pattern} pattern: {avg:.3f}')
+
+    #pattern = r'101'
+    #avg = calcAverageSearch(pattern, COINTOSSES)
+    #print(f'average search length for {pattern} pattern: {avg:.3f}')
+
+    patterns = ( f'{a:09b}'[-pattern_length:] for a in range(2**pattern_length) )
+    res = { pattern:calcAverageSearch(pattern, COINTOSSES) for pattern in patterns }
+    for ptrn, avg in res.items():
+        print(f'average search length for \'{ptrn}\' pattern is {avg:.3f} coin tosses')
 
 
-def calcAverageSearch(pattern, COINTOSSES):
-    tosses = ''.join(str(random.randint(0, 1)) for _ in range(COINTOSSES))
+def calcAverageSearch(pattern, cointosses=10_000):
+    tosses = ''.join(str(random.randint(0, 1)) for _ in range(cointosses))
     sequence_matches = re.split(pattern, tosses)[:-1]  # remove last element (not a match)
     average_search = mean(len(seq) for seq in sequence_matches) + len(pattern)
     return average_search
