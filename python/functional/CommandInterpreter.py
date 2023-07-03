45e12d9c8471375347d0d46ebba1bd1888c31fc8
Duane <duane.edmonds@gmail.com>
2023-07-02 23:08:40 -0700

    functional: updated command interpreter code

--- python/functional/CommandInterpreter.py
+++ python/functional/CommandInterpreter.py
@@ -4,13 +4,47 @@ from toolz import curry
 from typing import Tuple
 
 
-# evaluateOps :: Tuple[str] -> deque -> deque
+# built-in commands
+commands: dict = {
+    '+': add_f,
+    '-': subtract_f,
+}
+
+
+# unary command (float) decorator
+def cmdUnaryFloat(f: Callable[[], ]) -> Callable[[deque], deque]: 
+    def decoratedf(indeq: deque):
+        *rest, sa = indeq
+        a = int(sa)
+        outdeq = rest + deque(str(f(a))
+        return outdeq
+    return decoratedf
+
+@cmdUnaryFloat
+def add_f(a: float) -> float:
+    return f(a)
+
+
+# binary command (float) decorator
+def cmdBinaryFloat(f: Callable[[], ]) -> Callable[[deque], deque]: 
+    def decoratedf(indeq: deque):
+        *rest, sa, sb = indeq
+        a, b = map(int, (sa, sb))
+        outdeq = rest + deque(str(f(a, b))
+        return outdeq
+    return decoratedf
+
+@cmdBinaryFloat
+def add_f(a: float, b: float) -> float:
+    return a + b
+
+
 @curry
 def evaluateOps(ops: Tuple[str], indeq: deque) -> deque:
     def processOp(indeq: deque, op: str) -> deque:
         """
-        search command dictionary for command function to execute on
-        input deque - otherwise, add (value) to deque
+        search command dictionary for command function to execute
+        on input deque - otherwise, add (value) to deque
         """
 
         cmdf: Callable[[str], deque] = commands.get(op, None)
