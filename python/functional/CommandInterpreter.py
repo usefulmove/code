7d3c091cc2a4c85a7e6b82b05df0501ef89a476a
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-07 15:30:38 -0700

    functional: updated command interpreter code based on static analysis (mypy)

--- python/functional/CommandInterpreter.py
+++ python/functional/CommandInterpreter.py
@@ -21,7 +21,9 @@ def sqrt_f(a: float) -> float:
 
 
 # binary command (float) decorator
-def cmdBinaryFloat(f: Callable[[float, float], float]) -> Callable[[PDeque[str]], PDeque[str]]:
+def cmdBinaryFloat(
+    f: Callable[[float, float], float]
+) -> Callable[[PDeque[str]], PDeque[str]]:
     def decoratedf(indeq: PDeque[str]):
         *rest, sa, sb = indeq
         a, b = map(float, (sa, sb))
@@ -67,7 +69,7 @@ def evaluateOps(ops: Tuple[str], indeq: PDeque[str]) -> PDeque[str]:
         search command dictionary for command function to execute
         on input deque - otherwise, add (value) to deque
         """
-        cmdf: Callable[[PDeque[str]], PDeque[str]] = commands.get(op, None)
+        cmdf: Callable[[PDeque[str]], PDeque[str]] = commands.get(op, lambda d: d)
         return indeq.append(op) if cmdf is None else cmdf(indeq)
 
     # transformation of input deque performed by processing each op
