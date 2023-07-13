a85f9eb2a36abf3c100a42a417cfa892dff740b6
Duane <duane.edmonds@gmail.com>
2023-07-13 15:41:59 -0700

    (automated gcp push)

--- fib/fib.py
+++ fib/fib.py
@@ -3,28 +3,28 @@
 from functools import cache
 import sys
 
+
 # recursive solution
 @cache
 def fib(n: int) -> int:
-  match n:
-    case 0 | 1:
-      return n
-    case _:
-      return fib(n-1) + fib(n-2)
+    match n:
+        case 0 | 1: return n
+        case _: return fib(n-1) + fib(n-2)
+
 
 # tail recursive solution
 @cache
 def fib2(n: int, a: int = 0, b: int = 1) -> int:
-  match n:
-    case 0: return a
-    case 1: return b
-    case _: return fib2(n - 1, b, a + b)
+    match n:
+        case 0: return a
+        case 1: return b
+        case _: return fib2(n - 1, b, a + b)
 
 
 def main():
-  if len(sys.argv) > 1:
-    print(str(fib2(int(sys.argv[1]))))
+    if len(sys.argv) > 1:
+        print(str(fib2(int(sys.argv[1]))))
 
 
 if __name__ == '__main__':
-  main()
+    main()
