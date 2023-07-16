2a2efe8f15c518b3de0684bfd839219c51c74bd5
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-16 11:35:25 -0700

    scheme: minor updates to racket code

--- scheme/schemer.rkt
+++ scheme/schemer.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+; playground
 (define cube
     (lambda (a) (* a a a )))
 
@@ -12,3 +13,15 @@
     (lambda (a acc) (+ acc (square a)))
     0
     (range 1 (+ 8 1)))
+
+(define golden-ratio
+    (/ (- (sqrt 5) 1) 2))
+
+golden-ratio
+
+
+; define The Little Schemer atom? function
+(define atom?
+    (lambda (o) (and (not (pair? o)) (not (null? o)))))
+
+(atom? (quote ()))
