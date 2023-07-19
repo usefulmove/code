154dad576df7f4c0e0103bbd606cc3c38357f3f9
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-18 17:11:40 -0700

    fib: updated lisp implementations

--- fib/fib.rkt
+++ fib/fib.rkt
@@ -1,11 +1,12 @@
 (define (fib n)
-  (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2)))))
+  (if (< n 2)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
 
-(define (fibonacci2 n a b)
-  (if (= n 0)
-    a
-    (if (= n 1)
-      b 
-      (fibonacci2 (- n 1) b (+ a b)))))
-  
-(writeln (fib 38))
+(define (fib2 n [a 0] [b 1])
+  (cond
+       [(= n 0) a]
+       [(= n 1) b]
+       [else (fib2 (- n 1) b (+ a b))]))
+
+(displayln (fib2 38))
---
+++ fib/fib.scm
@@ -0,0 +1,14 @@
+(define (fib n)
+  (if (< n 2)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+(define (fib2 n)
+    (define (fibonacci2 x a b)
+        (cond
+            [(= x 0) a]
+            [(= x 1) b]
+            [else (fibonacci2 (- x 1) b (+ a b))]))
+    (fibonacci2 n 0 1))
+
+(display (fib2 38))
