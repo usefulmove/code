ab5887ad6d3acb04df4e35c51dbb8290ce8d5767
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-20 19:53:39 -0700

    scheme: added functional programming code to example file

--- scheme/code.rkt
+++ scheme/code.rkt
@@ -26,7 +26,7 @@
 (swap '(3 1 2 5 4) 1 3)  ; '(3 5 2 1 4)
 
 
-(define (square-root n)
+(define (square-root n)  ; example only (Babylonian method) - use (sqrt n) primative
     (define epsilon 0.000000001)
     (define (improve-guess guess)
         (cond
@@ -41,22 +41,38 @@
 
 ;; functional programming
 
-; function composition
-(define ())
+; filter map reduce
+(define seq '(1 2 3 4 5 6 7 8))
+(define (square a) (* a a))
 
+(filter odd? seq)  ; '(1 3 5 7)
+(map square seq)  ; '(1 4 9 16 25 36 49 64)
+(foldl + 0 seq)  ; 36
+
+
+; currying
+(define (add a)
+    (define (inner b) (+ a b)))
+
+(define add3 (add 3) n)
+(add3 2)  ; 5
+
+
+; function composition
+(define seq '(1 2 3 4 5 6 7 8))
+(define (square a) (* a a))
+
+(apply + (map square (filter odd? seq)))  ; 84
+
+(define (f lst) (filter odd? lst))
+(define (g lst) (map square lst))
+(define (h lst) (apply + lst))
+(define composed-f (compose h g f))
+(composed-f seq)  ; 84
 
 ; threading
-
-
-; filter map reduce
-(define (square a) (* a a))
-(apply + (map square (filter odd? seq)))
-
-(- (sqrt 10))
-((compose - sqrt) 10)
-
-(define (f seq) (filter odd? seq))
-(define (m seq) (map square seq))
-(define (r seq) (apply + seq))
-(define fmr (compose r m f))
-(fmr seq)
+(require threading)
+(~> seq
+    (f)
+    (g)
+    (h))  ; 84
