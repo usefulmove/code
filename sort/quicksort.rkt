9b9cee7c250f8552c6269d4059d7be44541ad57c
Duane <duane.edmonds@gmail.com>
2023-07-22 22:28:31 -0700

    updated quicksort algorithm and completed initial testing - ready for full debug

--- sort/quicksort.rkt
+++ sort/quicksort.rkt
@@ -7,16 +7,29 @@
 
 ; QuickSort algorithm
 (define (qsort lst)
+    (display "orig: ")
+    (displayln lst)
     (cond
         [(empty? lst) empty]
         [(equal? 1 (length lst)) lst]
-        (define pivot (car lst))  ; choose pivot
-        (define current (swap lst 0 (sub1 (length lst)))) ; move pivot to the end
-        (define ins 0)  ; insertion index
-        (TODO)
-    )
-)
+        [else
+            (define pivot (car lst))  ; choose pivot
+            (define current (swap lst 0 (sub1 (length lst)))) ; move pivot to the end
+            (define ins 0)  ; insertion index
+            (for ([i (in-range (sub1 (length current)))])
+                (if (< (list-ref current i) pivot)
+                    ; swap with insertion index and increment insertion index
+                    (set! current (swap current i ins))
+                    (set! ins (add1 ins))))
+            ; swap pivot with insertion index
+            (set! current (swap current ins (sub1 (length current))))
+            (display "current: ")
+            (displayln current)
+            ; recursively sort left and right sublists
+            (append (qsort (take current ins)) (list pivot) (qsort (drop current (add1 ins))))]))
 
-; playground
-(define test (list 9 3 7 2 1 5 4 6 8))
+; test case
+(define test (list 3 1 2 5 4 0))
+;(define test (list 9 3 7 2 1 5 4 6 8))
 
+(qsort test)
