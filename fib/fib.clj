9c1a47fd1236cd333f4456002be98e24fc026f4f
Duane <duane.edmonds@gmail.com>
2023-07-28 00:17:24 -0700

    fib: modified Clojure code to agree with Calva formatter

--- fib/fib.clj
+++ fib/fib.clj
@@ -1,12 +1,12 @@
 (defn fib [n]
-    (if (< n 2)
-        n
-        (+ (fib (- n 1)) (fib (- n 2)))))
+  (if (< n 2)
+    n
+    (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn fib2 [n]
-    (let [fibonacci2 (fn [n a b] (cond (= n 0) a
-                                       (= n 1) b
-                                       :else (fibonacci2 (- n 1) b (+ a b))))]
-      (fibonacci2 n 0 1)))
+  (let [fibonacci2 (fn [n a b] (cond (= n 0) a
+                                     (= n 1) b
+                                     :else (fibonacci2 (- n 1) b (+ a b))))]
+    (fibonacci2 n 0 1)))
 
 (println (fib2 38))
