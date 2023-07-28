5176ba38ead14f9262f14efb7146e8d7f55c77e5
Duane <duane.edmonds@gmail.com>
2023-07-28 00:16:14 -0700

    fib: updated Scheme and Clojure implementations

--- fib/fib.scm
+++ fib/fib.scm
@@ -4,11 +4,11 @@
       (+ (fib (- n 1)) (fib (- n 2)))))
 
 (define (fib2 n)
-    (define (fibonacci2 x a b)
-        (cond
-            [(= x 0) a]
-            [(= x 1) b]
-            [else (fibonacci2 (- x 1) b (+ a b))]))
-    (fibonacci2 n 0 1))
+    (letrec
+      ([fibonacci2 (lambda (x a b)
+                       (cond [(= x 0) a]
+                             [(= x 1) b]
+                             [else (fibonacci2 (- x 1) b (+ a b))]))])
+        (fibonacci2 n 0 1)))
 
 (display (fib2 38))
