5eeec65d61243f3b50e63fd8f22a4fdbfe8eef67
Duane <duane.edmonds@gmail.com>
2023-07-28 00:24:05 -0700

    fib: modified Clojure code to use letfn procedure

--- fib/fib.clj
+++ fib/fib.clj
@@ -4,9 +4,9 @@
     (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn fib2 [n]
-  (let [fibonacci2 (fn [n a b] (cond (= n 0) a
-                                     (= n 1) b
-                                     :else (fibonacci2 (- n 1) b (+ a b))))]
+  (letfn [(fibonacci2 [n a b] (cond (= n 0) a
+                                    (= n 1) b
+                                    :else (fibonacci2 (- n 1) b (+ a b))))]
     (fibonacci2 n 0 1)))
 
 (println (fib2 38))
