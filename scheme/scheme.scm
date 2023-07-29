70130c9a24cc98ac5fdf66409d40c80edd9465e8
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-28 19:46:23 -0700

    initial commit

---
+++ scheme/scheme.scm
@@ -0,0 +1,27 @@
+#lang racket
+
+#| REPL (read-eval-print-loop) |#
+
+
+#| definitions and evalutation - S-expressions |#
+; golden ratio
+
+
+#| procedures |#
+
+
+
+#| compound procedures |#
+; substitution model
+
+
+#| conditional expressions |#
+; if, cond
+
+
+#| recursion |#
+; factorial, fibonacci
+
+
+#| higher-order functions |#
+; map, filter, reduce
