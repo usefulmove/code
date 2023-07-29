e9a549863660516f8f05bf252c95cd533ab5aa04
Duane <duane.edmonds@gmail.com>
2023-07-28 22:54:09 -0700

    scheme: added koans

--- scheme/scheme.scm
+++ scheme/scheme.scm
@@ -7,8 +7,12 @@
 ; golden ratio
 
 
-#| procedures |#
+#| lists |#
+; cons, car, cdr, first, rest, last, list, null?, length, append, reverse
 
+
+#| procedures |#
+; define, lambda
 
 
 #| compound procedures |#
@@ -26,3 +30,22 @@
 
 #| higher-order functions |#
 ; map, filter, reduce
+
+
+#| koans (fill in the underscore) |#
+
+(= _ 8)
+
+(= (+ 3 2) _)
+
+(equal? '(_) (list 3 1 2))
+
+(equal? (car (list 3 1 2)) _)
+
+(equal? _ (cdr '(3 1 2)))
+
+(equal? (cons 0 (list 3 1 2)) _)
+
+(equal? 5 (_ '(3 1 2 4 6)))
+
+(equal? 5 (_ '(3 1 2 5)))
