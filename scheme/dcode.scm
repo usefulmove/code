d5b3e7c6f2967fa3275fc05f2310db04ad1c9ec8
Duane Edmonds <duane.edmonds@gmail.com>
2023-07-29 22:41:29 -0700

    minor refactor

--- scheme/dcode.scm
+++ scheme/dcode.scm
@@ -6,7 +6,7 @@
 (require racket/contract)
 
 
-;;; list operations ;;;
+#| list operations |#
 
 ; head :: [T] -> T
 (define head car)
@@ -14,9 +14,9 @@
 (define tail cdr)
 ; init :: [T] -> [T]
 (define (init lst)
-    (if (null? lst)
-        '()
-        (reverse (cdr (reverse lst)))))
+  (if (null? lst)
+      '()
+      (reverse (cdr (reverse lst)))))
 ; last :: [T] -> T  (built-in)
 ; fst :: [T] -> T
 (define fst car)
@@ -25,52 +25,52 @@
 
 ; swap :: [T] -> integer -> integer -> [T]
 (define (list-swap lst i j)
-    (let ([tmp (list-ref lst i)]
-          [out (list-set lst i (list-ref lst j))])
-      (list-set out j tmp)))
+  (let ([tmp (list-ref lst i)]
+        [out (list-set lst i (list-ref lst j))])
+    (list-set out j tmp)))
 
 ; display-list :: [T] -> null  ( side effect only )
 (define (display-list lst)
-    (for-each displayln lst))
+  (for-each displayln lst))
 
 ; find index of first instance of an item in a list (returns -1 if not found)
 ; list-index :: [T] -> T -> integer
 (define/contract (list-index item lst [index 0])
-    (-> exact-integer? (listof exact-integer?) exact-integer?)
-    (if (null? lst)
-        -1
-        (if (equal? item (car lst))
-            index
-            (list-index item (cdr lst) (add1 index)))))
+  (-> exact-integer? (listof exact-integer?) exact-integer?)
+  (if (null? lst)
+      -1
+      (if (equal? item (car lst))
+          index
+          (list-index item (cdr lst) (add1 index)))))
 
 
-;;; currying ;;;
+#| currying |#
 
 ; curry2 :: (T -> U -> V) -> (T -> (U -> V))
 (define (curry2 f)
-    (lambda (a) (lambda (b) (f a b))))
+  (lambda (a) (lambda (b) (f a b))))
 
 ; curry3 :: (T -> U -> V -> W) -> (T -> (U -> (V -> W)))
 (define (curry3 f)
-    (lambda (a) (lambda (b) (lambda (c) (f a b c)))))
+  (lambda (a) (lambda (b) (lambda (c) (f a b c)))))
 
 
 
 ; any? :: [T] -> (T -> boolean) -> boolean
 (define (any? f lst)
-    (if (null? lst)
-        #f
-    (if (f (car lst))
-        #t
-        (any? f (cdr lst)))))
+  (if (null? lst)
+      #f
+  (if (f (car lst))
+      #t
+      (any? f (cdr lst)))))
 
 ; all? :: [T] -> (T -> boolean) -> boolean
 (define (all? f lst)
-    (if (null? lst)
-        #t
-    (if (not (f (car lst)))
-        #f
-        (all? f (cdr lst)))))
+  (if (null? lst)
+      #t
+  (if (not (f (car lst)))
+      #f
+      (all? f (cdr lst)))))
 
 
 
@@ -79,23 +79,24 @@
 
 ; atom? :: object -> boolean
 (define (atom? obj)
-    (and (not (null? obj))
-         (not (pair? obj))))
+  (and (not (null? obj))
+       (not (pair? obj))))
 
 ; fixed-point convergence function
 ; converge-fixed-point :: (number -> number) -> number -> number
 (define/contract (converge-fixed-point f guess)
-    (-> (-> number? number?) number? number?)
-    (letrec (
-      [epsilon 0.000000001]
-      [close-enough? (lambda (a b) (> epsilon (abs (- a b))))]
-      [improve-guess (lambda (guess) (cond [(close-enough? guess (f guess)) (f guess)]
-                                           [else (improve-guess (f guess))]))])
-        (improve-guess guess)))
+  (-> (-> number? number?) number? number?)
+  (letrec
+    ([epsilon 0.000000001]
+     [close-enough? (lambda (a b) (> epsilon (abs (- a b))))]
+     [improve-guess (lambda (guess)
+                        (cond [(close-enough? guess (f guess)) (f guess)]
+                              [else (improve-guess (f guess))]))])
+      (improve-guess guess)))
 
 
 
-;;; debug helper functions ;;;
+#| debug helper functions |#
 
 ; debug-print :: T -> null  ( side effect only )
 (define-syntax-rule (debug-print var)
@@ -103,18 +104,18 @@
 
 ; debug-args
 (define (debug-args . args)
-    (for-each displayln args))
+  (for-each displayln args))
 
 
 
-;;; unit tests ;;;
+#| unit tests |#
 
 (unless (equal? (iota 8) '(1 2 3 4 5 6 7 8))
-    (error "error (dcode): iota failed"))
+  (error "error (dcode): iota failed"))
 
 (unless (let ([golden-ratio-est (converge-fixed-point
-                                 (lambda (a) (/ (add1 (sqr a)) (add1 (* a 2))))
-                                 1.0)]
-              [golden-ratio (/ (- (sqrt 5) 1) 2)])
-          (< (abs (- golden-ratio-est golden-ratio)) 0.001))
-    (error "error (dcode): converge-fixed-point failed"))
+                                    (lambda (a) (/ (add1 (sqr a)) (add1 (* a 2))))
+                                    1.0)]
+            [golden-ratio (/ (- (sqrt 5) 1) 2)])
+        (< (abs (- golden-ratio-est golden-ratio)) 0.001))
+  (error "error (dcode): converge-fixed-point failed"))
