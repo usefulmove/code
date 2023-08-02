8466977601ae9f553617ce51522f4d2e015de448
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-02 14:30:01 -0700

    dcode: added filter higher-order function

--- elisp/dcode.el
+++ elisp/dcode.el
@@ -1,13 +1,13 @@
 ;;; dcode.el --- Description -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2023 Duane
+;; Copyright (C) 2023 Robert Duane Edmonds
 ;;
-;; Author: Duane <dedmonds@G3>
-;; Maintainer: Duane <dedmonds@G3>
+;; Author: Duane Edmonds <duane.edmonds@gmail.com>
+;; Maintainer: Duane Edmonds <duane.edmonds@gmail.com>
 ;; Created: August 02, 2023
 ;; Modified: August 02, 2023
 ;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Keywords:
 ;; Homepage: https://github.com/dedmonds/dcode
 ;; Package-Requires: ((emacs "24.3"))
 ;;
@@ -19,10 +19,7 @@
 ;;
 ;;; Code:
 
-
-
 (provide 'dcode)
-;;; dcode.el ends here
 
 ; foldl :: (U -> T -> U) -> U -> [T] -> U
 (defun foldl (f acc lst)
@@ -30,11 +27,26 @@
       acc
       (foldl f (funcall f acc (car lst)) (cdr lst))))
 
-(foldl (lambda (acc a) (+ acc a)) 0 '(3 1 2 5 4))
-
+; map :: (T -> T) -> [T] -> [T]
 (defun map (f lst)
   (if (null lst)
       lst
       (cons (funcall f (car lst)) (map f (cdr lst)))))
 
-(map (lambda (a) (* a a)) '(3 1 2 5 4))
+; filter :: (T -> boolean) -> [T] -> [T]
+(defun filter (f lst)
+  (if (null lst)
+      lst
+      (if (not (funcall f (car lst)))
+          (filter f (cdr lst))
+          (cons (car lst) (filter f (cdr lst))))))
+
+; even? :: number -> booleean
+(defun even? (n) (= 0 (mod n 2)))
+;
+; odd? :: number -> booleean
+(defun odd? (n) (not (even? n)))
+
+(foldl (lambda (acc a) (+ acc a)) 0 (map (lambda (a) (* a a)) '(1 2 3 4 5 6 7 8)))  ; 204
+
+;;; dcode.el ends here
