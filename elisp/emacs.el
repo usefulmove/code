7b36ece379cb093b8fe9bae44fe9dc4473bc6d5c
Duane <duane.edmonds@gmail.com>
2023-08-02 21:50:39 -0700

    added emacs window key bindings

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -1,1 +1,8 @@
 (describe-bindings)
+
+; emacs windows
+; C-x o  switch cursor to another window
+; C-x 2  split frame above and below
+; C-x 3  split frame side by side
+; C-x 0  delete current window
+; C-x 1  delete all other windows
