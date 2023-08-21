59b14e5f7a690ef6274932e5a98676828ca284bc
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-21 16:07:23 -0700

    ...

---
+++ clojure/memo.clj
@@ -0,0 +1,11 @@
+(ns )
+
+; memo :: (T -> U) -> (T -> U)
+; memoization (cache)
+(defn memo [f]
+  (let [cache {}]
+    (fn [arg] (if (contains? cache arg)    ; if cache contains the passed argument
+                  (arg cache)              ; return cached value
+                  (let [res (f arg)]       ; otherwise...
+                    (update cache arg res) ; add value to cache
+                    res)))))               ; and return value
