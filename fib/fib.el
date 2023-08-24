255dc2fe0729eb2ffa1c1385df2e38e73f0d4aff
Duane Edmonds <duane.edmonds@gmail.com>
2023-08-23 21:01:19 -0700

    ...

--- fib/fib.el
+++ fib/fib.el
@@ -1,15 +1,12 @@
 ; default solution
 (defun fib (n)
-  (if (< n 2)
-      n
-      (+ (fib (- n 1)) (fib (- n 2)))))
+  (cond ((< n 2) n)
+        (t (+ (fib (- n 1)) (fib (- n 2))))))
 
 ; tail recursion
 (defun fibonacci2 (n a b)
-  (if (equal 0 n)
-       a
-       (if (equal 1 n)
-           b
-           (fibonacci2 (- n 1) b (+ a b)))))
+  (cond ((= 0 n) a)
+        ((= 1 n) b)
+        (t (fibonacci2 (- n 1) b (+ a b)))))
 
 (defun fib2 (n) (fibonacci2 n 0 1))
