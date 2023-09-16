124f0a13a31f66270622ede2e0b6468499782d44
Duane <duane.edmonds@gmail.com>
2023-09-16 14:36:04 -0700

    removed unused files

--- languages/elisp/emacs.el
+++
@@ -1,8 +0,0 @@
-(describe-bindings)
-
-; emacs windows
-; C-x o  switch cursor to another window
-; C-x 2  split frame above and below
-; C-x 3  split frame side by side
-; C-x 0  delete current window
-; C-x 1  delete all other windows
