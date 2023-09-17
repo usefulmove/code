de64fb253066fdba912a110927880446ea270998
Duane <duane.edmonds@gmail.com>
2023-09-16 21:24:22 -0700

    corrected reset problem. updated not to rely on recursion.

--- cointoss/cointoss.el
+++ cointoss/cointoss.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Duane Edmonds <duane.edmonds@gmail.com>
 ;; Created: September 16, 2023
 ;; Modified: September 16, 2023
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Keywords: tools
 ;; Homepage: https://github.com/dedmonds/cointoss
 ;; Package-Requires: ((emacs "24.3"))
@@ -26,31 +26,31 @@
 (defun make-coins()
   (require 'cl-lib)
   (let ((coins '()))
-    (lambda ()
-      (setq coins (cons (cl-random 2)
-                        (take 2 coins)))
+    (lambda (cmd)
+      (cond ((equal? :flip cmd) (setq coins (cons (cl-random 2)
+                                              (take 2 coins))))
+            ((equal? :reset cmd) (setq coins '())))
       coins)))
 
 
-(defun toss-and-check-matches (pattern coins tosses matches)
-  (if (= 0 tosses) matches
-      (if (equal? pattern (call coins))
-          (toss-and-check-matches pattern coins (dec tosses) (inc matches))
-          (toss-and-check-matches pattern coins (dec tosses) matches))))
+(defun toss-and-check-matches (pattern coins tosses)
+  (let ((matches 0))
+    (dotimes (i tosses)
+      (when (equal? pattern (call coins :flip))
+        (setq matches (inc matches))
+        (call coins :reset)))
+    matches))
 
 
 (defun run-simulation (pattern tosses)
   (let ((coins (make-coins)))
-    (toss-and-check-matches pattern coins tosses 0)))
+    (toss-and-check-matches pattern coins tosses)))
 
 
 ;; simulation
-(setq max-lisp-eval-depth 1000000)
-(setq max-specpdl-size 1000000)
-
-(setq cointosses 10000)
-(/ cointosses (run-simulation '(1 0 1) cointosses) 1.0)
-(/ cointosses (run-simulation '(0 1 1) cointosses) 1.0)
+(setq cointosses 1000000)
+(/ cointosses (run-simulation '(1 0 1) cointosses) 1.0) ; 9.983427510332847
+(/ cointosses (run-simulation '(0 1 1) cointosses) 1.0) ; 7.980145398249156
 
 
 
