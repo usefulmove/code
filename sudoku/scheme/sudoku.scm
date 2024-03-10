1f4ac00c3fc20b1564f53cfd7641114d61435547
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-10 13:21:25 -0700

    added scheme project

---
+++ sudoku/scheme/sudoku.scm
@@ -0,0 +1,25 @@
+#lang racket
+
+
+(define board
+  (list
+   (list 5 3 0 0 7 0 0 0 0)
+   (list 6 0 0 1 9 5 0 0 0)
+   (list 0 9 8 0 0 0 0 6 0)
+   (list 8 0 0 0 6 0 0 0 3)
+   (list 4 0 0 8 0 3 0 0 1)
+   (list 7 0 0 0 2 0 0 0 6)
+   (list 0 6 0 0 0 0 2 8 0)
+   (list 0 0 0 4 1 9 0 0 5)
+   (list 0 0 0 0 8 0 0 7 9)))
+
+
+; (list-ref (list-ref board 1) 5) => 5
+
+; (read-pos 1 5) => 5
+
+(define read-pos
+  (lambda (rank file)
+    ((list-ref (list-ref board (- 9 rank)) file))))
+
+
