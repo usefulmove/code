589449ff76b6d6bd2bcbfbbc9f2aa8c436324a8e
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-16 04:51:37 -0700

    ...

--- sudoku/scheme/sudoku.scm
+++ sudoku/scheme/sudoku.scm
@@ -15,13 +15,15 @@
    (list 0 0 0 0 8 0 0 7 9)))
 
 
-; get-value :: int -> int -> int
-(define get-value (lambda (board rank file)
-                    (list-ref
+; get-value :: [[int]] -> (int . int) -> int
+(define get-value (lambda (board pos)
+                    (let ((rank (car pos))
+                          (file (cdr pos)))
                       (list-ref
-                      	board
-                      	(- 8 rank))
-                      file)))
+                        (list-ref
+                        	board
+                        	(- 8 rank))
+                        file))))
 
 
 ; set-value :: [[int]] -> (int . int) -> int -> [[int]]
@@ -30,26 +32,44 @@
     (todo)))
 
 
-; solve :: [[int]] -> [[int]] (todo)
+; solve :: [[int]] -> [int] -> [[int]] (todo)
 (define solve
-  (lambda (board)
+  (lambda (board lst-to-attempt-orig)
     (let ((first-zero-pos (get-first-zero-pos board)))
       (if (= -1 (car first-zero-pos))
         board ; solution found. return board.
-        (let ((possibles (get-possibles board first-zero-pos)))
-          (if (empty? possibles)
-            empty ; no solution exists
-            (solve
-              (set-value board first-zero-pos (car possibles)))))))))
+        (let ((valid-values (get-valid-values board first-zero-pos))
+              (lst-to-attempt (todo)))
+          (if (empty? valid-values)
+            '() ; no valid solution exists
+            (solve ; (todo)
+              (set-value board first-zero-pos (car lst-to-attempt))
+              (cdr lst-to-attempt-orig))))))))
 
 
-; get-possibles :: [[int]] -> (int . int) -> [int]
-(define get-possibles
+; get-valid-values :: [[int]] -> (int . int) -> [int]
+(define get-valid-values
   (lambda (board pos)
-    (todo)))
+    (let* ((rank (car pos))
+           (file (cdr pos))
+           (cell (todo))
+           (get-valid-values-rank (lambda (board file)
+                                    (todo)))
+           (get-valid-values-file (lambda (board rank)
+                                    (todo)))
+           (get-valid-values-cell (lambda (board cell)
+                                    (todo))))
+      (if (zero? (get-value board pos))
+        (set->list (set-intersect (get-valid-values-rank board rank)
+                                  (get-valid-values-file board file)
+                                  (get-valid-values-cell board cell)))
+        '()))))
 
 
 ; get-first-zero-pos :: [[int]] -> (int . int)
 (define get-first-zero-pos
   (lambda (board)
     (todo)))
+
+
+(solve board (range 1 10))
