c37050adc6d62fcd4c00ec5bc7c01eb5dc60c024
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-26 11:18:49 -0700

    adding scheme forms file

---
+++ languages/scheme/scheme-forms.scm
@@ -0,0 +1,29 @@
+#|     Scheme basic forms     |#
+
+
+(if (predicate-expression)
+    (then-expression)
+    (else-expression))
+
+(cond (((predicate-1) (expression-1))
+       ((predicate-2) (expression-2))
+       (else (else-expression))))
+
+(and (predicate-1)
+     (predicate-2))
+
+(or (predicate-1)
+    (predicate-2))
+
+(define variable-name (value-expression))
+
+(define function-name
+  (lambda (argument-1 argument-2)
+    (expression)))
+
+(let ((variable-1 (expression-1))
+      (variable-2 (expression-2)))
+  (body-expression))
+
+(begin (expression-1)
+       (expression-2))
