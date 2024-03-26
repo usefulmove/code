e67677b2a9aa1708b66088753e319f446334f274
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-26 06:59:00 -0700

    updated Racket code to make use of default args

--- fib/fib.rkt
+++ fib/fib.rkt
@@ -1,17 +1,15 @@
 #lang racket
 
+
 (define (fib n)
-  (cond [(< n 2) n]
-        [else (+ (fib (- n 1))
-                 (fib (- n 2)))]))
+  (cond ((< n 2) n)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
 
 
-(define (fib2 n)
-    (letrec
-      ([fibonacci2 (lambda (n a b)
-                       (cond [(= n 0) a]
-                             [(= n 1) b]
-                             [else (fibonacci2 (- n 1) b (+ a b))]))])
-        (fibonacci2 n 0 1)))
+(define (fib2 n (a 0) (b 1))
+  (cond ((= n 0) a)
+        ((= n 1) b)
+        (else (fib2 (- n 1) b (+ a b)))))
 
 (display (fib2 38))
