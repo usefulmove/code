2217190d880de5125996666503df5abd2eae8660
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-27 12:37:01 -0700

    refactor for clean up

--- cointoss/cointoss.rkt
+++ cointoss/cointoss.rkt
@@ -9,27 +9,32 @@
                                 (let* ((pattern-matches (car acc))
                                        (toss-minus-2 (cadr acc))
                                        (toss-minus-1 (caddr acc))
-                                       (pattern-to-compare (list toss-minus-2
+                                       (current-sequence (list toss-minus-2
                                                                  toss-minus-1
                                                                  current-toss)))
                                   (cond ((or (= no-toss
                                                 toss-minus-2)
                                              (= no-toss
-                                                toss-minus-1)) (list pattern-matches
-                                                                     toss-minus-1
-                                                                     current-toss))
-                                        ((equal? pattern ; pattern matches - reset
-                                                 pattern-to-compare) (list (add1 pattern-matches)
-                                                                           no-toss
-                                                                           no-toss))
+                                                toss-minus-1))
+                                           (list pattern-matches
+                                                 toss-minus-1
+                                                 current-toss))
+                                        ((equal? pattern ; pattern matches
+                                                 current-sequence)
+                                           (list (add1 pattern-matches) ; reset
+                                                 no-toss
+                                                 no-toss))
                                         (else (list pattern-matches
                                                     toss-minus-1
                                                     current-toss)))))
-                              (list 0 no-toss no-toss) ; (pattern-matches toss-minus-2 toss-minus-1)
+                              (list 0        ; pattern-matches
+                                    no-toss  ; toss-minus-2
+                                    no-toss) ; toss-minus-1
                               (map
                                generate-toss
                                (range total-tosses))))))
-    (exact->inexact (/ total-tosses total-matches))))
+    (exact->inexact (/ total-tosses
+                       total-matches))))
 
 
 (cointoss '(0 1 1) 10000000)
