d2f5cf9b8a14b0bc5a684618aa83b03baaf9e846
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-27 19:23:02 -0700

    added continuation code example

---
+++ languages/scheme/continuations.scm
@@ -0,0 +1,13 @@
+#lang racket
+
+(define (sum-up-to-threshold numbers threshold)
+  (call/cc (lambda (return-to-context)
+             (foldl
+              (lambda (a acc)
+                (if (> acc threshold)
+                    (return-to-context acc)
+                    (+ acc a)))
+              0
+              numbers))))
+
+(sum-up-to-threshold '(1 2 3 4 5 6 7 8 9 10) 15) ; 21
