5d0d6fc5db35e5bb1d96531a4c67d705f06eb651
Duane Edmonds <duane.edmonds@gmail.com>
2024-03-30 18:59:57 -0700

    commiting working solution

--- sudoku/sudoku.rkt
+++ sudoku/sudoku.rkt
@@ -25,6 +25,28 @@
         0 0 0 4 1 9 0 0 5
         0 0 0 0 8 0 0 7 9 ))
 
+;(define original-board
+;  (list 0 3 0 0 0 0 0 2 0
+;        0 0 7 0 0 0 5 0 0
+;        0 0 1 2 3 9 7 0 0
+;        0 8 9 0 4 0 1 5 0
+;        0 0 0 0 0 0 0 0 0
+;        0 5 0 0 0 0 0 4 0
+;        0 0 2 7 0 1 4 0 0
+;        8 0 0 0 9 0 0 0 6
+;        0 7 0 3 5 8 0 1 0 ))
+
+; easy
+;(define original-board
+;  (list 5 4 6 0 0 0 0 0 1
+;        0 0 0 0 0 1 0 0 4
+;        0 0 0 5 6 4 0 0 8
+;        0 5 2 0 4 0 1 0 0
+;        0 0 7 6 0 3 4 0 0
+;        0 0 8 0 5 0 2 9 0
+;        7 0 0 9 8 2 0 0 0
+;        6 0 0 4 0 0 0 0 0
+;        9 0 0 0 0 0 3 4 2 ))
 
 (define (display-board board)
   (displayln (take board 9))
@@ -141,9 +163,11 @@
 ;; solved? :: board -> boolean
 ;;         :: [int] -> boolean
 (define (solved? board)
-  (not (member 0 board)))
+  (not (or (null? board)
+           (member 0 board))))
 
 
+;; backtracking solver
 ;; solve :: board -> board
 ;; solve :: [int] -> [int] (empty list if fails to solve the board)
 (define (solve board)
@@ -155,7 +179,8 @@
            (begin
              (for ((candidate (range 1 (add1 9))))
                (if (possible? board empty-cell candidate)
-                   (let ((possible-solution (solve(set-cell-value board empty-cell candidate))))
+                   (let ((possible-solution
+                          (solve(set-cell-value board empty-cell candidate))))
                      (if (solved? possible-solution)
                          (return possible-solution) ; return solution.
                          (void)))
@@ -164,4 +189,4 @@
 
 
 
-(solve original-board)
+(display-board (solve original-board))
