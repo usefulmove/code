93653ffcf95d6a2512f468abade49b09b156cfb0
Duane Edmonds <duane.edmonds@gmail.com>
2024-04-07 15:45:24 -0700

    ..

--- languages/elisp/luhn.el
+++ languages/elisp/luhn.el
@@ -1,27 +1,28 @@
 ;; Luhn algorithm
 
-(load-file "~/repos/cora/src/cora.el")
+(load-file "~/repos/othello/src/othello.el")
 
 (defun luhn (s)
   (let* ((digits (map
                   (lambda (c) (char-to-int c))
                   (string-to-list s)))
-         (sum (cdr (foldr
-                     (lambda (acc digit)
-                       (let ((index (car acc))
-                             (total (cdr acc))
-                             (double-and-set (lambda (n)
-                                               (let ((double (+ n n)))
-                                                 (if (> double 9)
-                                                     (- double 9)
-                                                   double)))))
-                         (if (even? index)
-                             (cons (inc index)
-                                   (+ total digit))
-                           (cons (inc index)
-                                 (+ total (call double-and-set digit))))))
-                     '(0 . 0) ; (index . total)
-                     digits))))
-    (zero? (mod sum 10))))
+         (sum
+          (cdr (o-fold-right
+                (lambda (acc digit)
+                  (let ((index (car acc))
+                        (total (cdr acc))
+                        (double-and-set (lambda (n)
+                                         (let ((double (+ n n)))
+                                           (if (> double 9)
+                                               (- double 9)
+                                             double)))))
+                    (if (even? index)
+                        (cons (inc index)
+                              (+ total digit))
+                      (cons (inc index)
+                            (+ total (call double-and-set digit))))))
+                '(0 . 0) ; (index . total)
+                digits))))
+    (o-zerop (mod sum 10))))
 
 (luhn "79927398713")
