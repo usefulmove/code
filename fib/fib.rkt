c9324e4bea4bf67bd701c0c5c8ba6d569fa19be3
Duane Edmonds <duane.edmonds@gmail.com>
2024-04-13 23:34:38 -0700

    added case-lambda versions in scheme and racket

--- fib/fib.rkt
+++ fib/fib.rkt
@@ -12,4 +12,13 @@
         ((= n 1) b)
         (else (fib2 (- n 1) b (+ a b)))))
 
-(display (fib2 38))
+
+(define fib3
+  (case-lambda
+    ((n) (fib3 n 0 1))
+    ((n a b) (cond ((= n 0) a)
+                   ((= n 1) b)
+                   (else (fib3 (- n 1) b (+ a b)))))))
+
+
+(display (fib3 38))
--- fib/fib.scm
+++ fib/fib.scm
@@ -12,4 +12,12 @@
                              (else (fibonacci2 (- n 1) b (+ a b)))))))
         (fibonacci2 n 0 1)))
 
-(display (fib2 38))
+(define fib3
+  (case-lambda
+    ((n) (fib3 n 0 1))
+    ((n a b) (cond ((= n 0) a)
+                   ((= n 1) b)
+                   (else (fib3 (- n 1) b (+ a b)))))))
+
+(display (fib3 38))
+(newline)
