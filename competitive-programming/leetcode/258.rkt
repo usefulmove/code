b3b472c4e78357b0445a67a6efb978191d9fb004
Duane Edmonds <duane.edmonds@gmail.com>
2024-04-15 19:18:02 -0700

    ...

--- competitive-programming/leetcode/258.rkt
+++ competitive-programming/leetcode/258.rkt
@@ -1,18 +1,21 @@
 #lang racket
 
+
 (define (add-digits num)
-  (let* ((char->value (lambda (c)
-                        (- (char->integer c)
-                           (char->integer #\0))))
-         (digits (map
-                   char->value
-                   (string->list (number->string num))))
-         (sum (lambda (lst)
-                (apply + lst)))
-         (total (sum digits)))
-    (if (< total 10)
-        total
-        (add-digits total))))
+  (let* ((reduce (lambda (n)
+                   (foldl
+                    (lambda (char acc)
+                      (+ acc (- (char->integer char)
+                                (char->integer #\0))))
+                    0
+                    (string->list (number->string n)))))
+         (reduced (reduce num)))
+    (if (< reduced 10)
+        reduced
+        (add-digits reduced))))
+
 
 (add-digits 38)
 (add-digits 0)
+(add-digits 06181985)
+(add-digits 12101975)
