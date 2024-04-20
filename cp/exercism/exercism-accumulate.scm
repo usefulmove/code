9d90a1bcdcca869961be8e1279f8a69d169787a2
Duane Edmonds <duane.edmonds@gmail.com>
2024-04-19 21:54:36 -0700

    ...

--- cp/exercism/exercism-accumulate.scm
+++
@@ -1,9 +0,0 @@
-(define (accumulate f xs)
-  (cond ((null? xs) '())
-        (else (cons (f (car xs))
-                    (accumulate f (cdr xs)))))) 
-
-
-(accumulate (lambda (a) (* a a)) '(1 2 3)) 
-(accumulate string-upcase '("this" "that" "other"))
-(accumulate (lambda (s) (list->string (reverse (string->list s)))) '("this" "that" "other")) 
