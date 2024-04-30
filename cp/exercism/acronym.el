2b861ed5484f34db7da8032004192562319749e5
Duane Edmonds <duane.edmonds@gmail.com>
2024-04-30 09:16:11 -0700

    ...

--- cp/exercism/acronym.el
+++ cp/exercism/acronym.el
@@ -6,19 +6,20 @@
 
 
 (defun acronym (phrase)
-  (let ((words (string-split phrase "[ \f\t\n\r\v\-]+"))
+  (let ((words (split-string phrase "[ \f\t\n\r\v\-]+"))
         (char-alpha-p (lambda (c)
                         (let ((c (downcase c)))
                           (<= ?a c ?z)))))
     (mapconcat
      (lambda (word)
-       (letrec ((recurse (lambda (lst)
+       (letrec ((find-letter (lambda (lst)
                            (cond ((null lst) "")
                                  ((funcall char-alpha-p (car lst))
                                   (upcase (string (car lst))))
-                                 (t (funcall recurse (cdr lst)))))))
-         (funcall recurse (string-to-list word))))
-     words)))
+                                 (t (funcall find-letter (cdr lst)))))))
+         (funcall find-letter (string-to-list word))))
+     words
+     "")))
 
 
 (provide 'acronym)
