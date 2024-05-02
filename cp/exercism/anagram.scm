54f5d5960eb6b0bca1efe0ca1de17c875f38b32f
Duane Edmonds <duane.edmonds@gmail.com>
2024-05-01 19:15:34 -0700

    ...

--- cp/exercism/anagram.scm
+++ cp/exercism/anagram.scm
@@ -1,17 +1,14 @@
 (import (rnrs))
 
 (define (anagram target words)
-  (fold-left
-   (lambda (acc word)
+  (filter
+   (lambda (word)
      (let ((normalise (lambda (word)
                         (sort char<? (map
                                       char-downcase
                                       (string->list word))))))
-       (if (and (not (equal? (string-downcase target)
-                             (string-downcase word)))
-                (equal? (normalise target)
-                        (normalise word)))
-           (cons word acc) ; add anagram to accumulator
-           acc)))
-   '()
+       (and (not (equal? (string-downcase target)
+                         (string-downcase word)))
+            (equal? (normalise target)
+                    (normalise word)))))
    words))
