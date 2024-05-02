7d1e967eb5486d9c5684f8c5bb17a76fad3d85ac
Duane Edmonds <duane.edmonds@gmail.com>
2024-05-01 21:06:32 -0700

    refactored thread macro to no longer require quoting of forms

--- languages/scheme/thread.scm
+++ languages/scheme/thread.scm
@@ -1,14 +1,10 @@
 (define-syntax thread
-  (syntax-rules ()
+  (syntax-rules (eval)
     ((thread seed) seed)
-    ((thread seed form more ...)
-     (thread ((eval (create-lambda form)) seed) more ...))))
-
-
-(define (create-lambda sexp)
-  `(lambda (_arg_)
-    ,(replace-underscore sexp '_arg_)))
-
+    ((thread seed form)
+     (eval (replace-underscore 'form seed)))
+    ((thread seed form1 form2 ...)
+     (thread (thread seed form1) form2 ...))))
 
 (define (replace-underscore sexp value)
   (cond ((eq? sexp '_) value)
@@ -17,46 +13,10 @@
                     (replace-underscore (cdr sexp) value)))))
 
 
-#;(begin
-  (display (replace-underscore '(* _ _ _) 8))
-  (newline))
-#;(begin
-  (display (eval (replace-underscore '(* _ _ _) 8)))
-  (newline))
-#;(begin 
-  (display (let ((f (create-lambda '(/ (- (sqrt _) 1) 2))))
-             ((eval f) 5)))
-  (newline))
-
-
-; prototype(s)
 (begin 
   (display
    (thread 5
-    '(sqrt _)
-    '(- _ 1)
-    '(/ _ 2)
-    '(/ 1 _)
-    '(/ 1 _)))
-  (newline))
-
-#;(begin 
-  (display 
-   (expand '(thread 5
-             '(sqrt _)
-             '(- _ 1))))
-  (newline))
-
-#;(begin
-  (display
-   (thread 8
-    '(* _ _ _)))
-  (newline))
-
-#;(begin
-  (display
-   (thread 8
-    '(iota (add1 _))
-    '(map (lambda (a) (* a a)) _)
-    '(apply + _)))
+    (sqrt _)
+    (- _ 1)
+    (/ _ 2)))
   (newline))
