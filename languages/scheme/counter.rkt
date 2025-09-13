5530e9e3d3bc3d796c32ec5cedb09c2a912b30fc
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-12 20:02:00 -0700

    ...

---
+++ languages/scheme/counter.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define counter (lambda (lst)
+                  (let loop ((cs lst)
+                             (cnts '())) ; assoc list
+                    (if (empty? cs)
+                        (reverse cnts)
+                        (let* ((c (car cs))
+                               (ind (index-of (map car cnts) c)))
+                          (loop (cdr cs)
+                                (if ind
+                                    (append (take cnts ind)
+                                            (let ((c-cnt (cdr
+                                                          (list-ref cnts ind))))
+                                              (list (cons c (+ c-cnt 1))))
+                                            (drop cnts (+ ind 1)))
+                                    (cons (cons c 1) cnts))))))))
+
+(counter (string->list "this is a test."))
