7526048b415e82886f8444739c8242651b56df23
Duane Edmonds <duane.edmonds@gmail.com>
2025-09-27 16:53:06 -0700

    ...

---
+++ cointoss/cointoss2.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define (cointoss pattern tosses)
+  (let ((toss (lambda () (random 2)))
+        (pattern-length (length pattern)))
+    (let loop ((current-toss tosses)
+               (history '())
+               (matches 0.0))
+      (cond ((zero? current-toss) ; done
+             (if (zero? matches)
+                 -1
+                 (/ tosses matches))) ; analyze
+            ((< (length history) pattern-length)
+             (loop (- current-toss 1)
+                   (cons (toss) history)
+                   matches))
+            ((equal? pattern history) ; match
+             (loop (- current-toss 1)
+                   (list (toss)) ; reset
+                   (+ matches 1)))
+            (else
+             (loop (- current-toss 1)
+                   (cons (toss) (take history (- pattern-length 1)))
+                   matches))))))
+
+(let ((tosses 8000000)
+      (show (lambda (a)
+              (displayln (/ (round (* a 100)) 100.0)))))
+  (show (cointoss '(1 1 1) tosses))
+  (show (cointoss '(1 0 1) tosses))
+  (show (cointoss '(1 1 0) tosses)))
