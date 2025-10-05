ff19b07bd11db39fbfd2d4b177dbc631ca5e36a4
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-04 18:56:59 -0700

    ...

--- languages/scheme/counter.rkt
+++ languages/scheme/counter.rkt
@@ -1,20 +1,26 @@
 #lang racket
 
-(define counter (lambda (lst)
-                  (let loop ((cs lst)
-                             (cnts '())) ; assoc list
-                    (if (empty? cs)
-                        (reverse cnts)
-                        (let* ((c (car cs))
-                               (ind (index-of (map car cnts) c)))
-                          (loop (cdr cs)
-                                (if ind
-                                    (append (take cnts ind)
-                                            (let ((c-cnt (cdr
-                                                          (list-ref cnts ind))))
-                                              (list (cons c (+ c-cnt 1))))
-                                            (drop cnts (+ ind 1)))
-                                    (cons (cons c 1) cnts))))))))
+(define counter
+  (lambda (elems)
+    (reverse (foldl
+              (lambda (c cnts)
+                (let ((ind (index-of (map car cnts) c)))
+                  (if ind
+                      (append (take cnts ind)
+                              (let ((c-cnt (cdr (list-ref cnts ind))))
+                                (list (cons c (+ c-cnt 1))))
+                              (drop cnts (+ ind 1)))
+                      (cons (cons c 1) cnts))))
+              '() ; assoc list
+              elems))))
 
-(counter (string->list "this is a test.")) ; =>
-; '((#\t . 3) (#\h . 1) (#\i . 2) (#\s . 3) (#\space . 3) (#\a . 1) (#\e . 1) (#\. . 1))
+(counter (string->list "this is a test."))
+; =>
+; '((#\t . 3)
+;  (#\h . 1)
+;  (#\i . 2)
+;  (#\s . 3)
+;  (#\space . 3)
+;  (#\a . 1)
+;  (#\e . 1)
+;  (#\. . 1))
