858fd31405edfa9e5004ae6d52881953151ccb92
Duane Edmonds <duane.edmonds@gmail.com>
2026-01-07 22:43:20 -0800

    updated ocaml

--- fib/fib.ml
+++ fib/fib.ml
@@ -4,11 +4,13 @@ let rec fib n =
   | 1 -> 1
   | _ -> fib (n - 1) + fib (n - 2)
 
-let rec fib2 n a b =
-  match n with
-  | 0 -> a
-  | 1 -> b
-  | _ -> fib2 (n - 1) b (a + b)
+let rec fib2 n =
+  let rec fib2_helper n a b =
+    match n with
+    | 0 -> a
+    | 1 -> b
+    | _ -> fib2_helper (n - 1) b (a + b)
+  in fib2_helper n 0 1
 
 let () =
-  print_int (fib2 38 0 1)
+  print_int (fib2 38)
