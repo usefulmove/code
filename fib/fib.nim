e64389403e4487f1552fdfcf250c5ff556a04f5b
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-01 17:18:43 -0800

    updated nim.

--- fib/fib.nim
+++ fib/fib.nim
@@ -5,4 +5,15 @@ proc fib(n: int): int =
     else:
         fib(n - 1) + fib(n - 2)
 
-echo fib(38)
+
+proc fib2(n: int, a: int = 0, b: int = 1): int =
+    case n
+    of 0:
+        a
+    of 1:
+        b
+    else:
+        fib2(n - 1, b, a + b)
+
+
+echo fib2(38)
