e9359a796858daa4feee4eb30358e20e05a9dbd7
Duane Edmonds <duane.edmonds@gmail.com>
2026-03-01 19:30:21 -0800

    initial commit

---
+++ languages/rust/lc/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let filename = std::env::args().nth(1).expect("no file given");
+    let read_result = std::fs::read_to_string(&filename);
+
+    match read_result {
+        Ok(contents) => {
+            println!("  file:  {}", filename);
+            println!("  lines: {}", contents.lines().count());
+        },
+        Err(e) => println!("  error: {}", e),
+    }
+}
